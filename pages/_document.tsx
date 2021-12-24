2118d46c055e652bb4512997b5977af043bc7bd1
TrpFrog <dev@trpfrog.net>
2021-12-24 23:19:46 +0900

    Embed webfont to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Favicon from '../components/head/Favicon'
-import WebFont from "../components/head/WebFont";
 
 const MyDocument = () => {
     return (
@@ -8,7 +7,13 @@ const MyDocument = () => {
             <Head>
                 <meta charSet="utf-8" />
                 <Favicon />
-                <WebFont />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;800&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
