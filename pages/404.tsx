84959f170e9b155b9f059abca3c947d4cf85326b
TrpFrog <dev@trpfrog.net>
2021-12-25 00:11:53 +0900

    Add 404

---
+++ pages/404.tsx
@@ -0,0 +1,33 @@
+import {NextPage} from "next";
+import Layout from "../components/Layout";
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/404/404.module.css';
+
+const TrpFrog404: NextPage = () => {
+    return (
+        <>
+            <Layout>
+                <div id={styles.title}>
+                    <h1>404 Not Found</h1>
+                    <p>
+                        <Image
+                            src="/images/404.png"
+                            width="332"
+                            height="332"
+                            alt="404の画像"
+                        />
+                    </p>
+                    <p>このページは存在しません。</p>
+                    <p>サイトの工事中、またはリンクの誤りの可能性があります。</p>
+                    <p>
+                        トップページに戻る場合は<Link href="/"><a>ここ</a></Link>
+                        をクリックしてください。
+                    </p>
+                </div>
+            </Layout>
+        </>
+    )
+}
+
+export default TrpFrog404;
