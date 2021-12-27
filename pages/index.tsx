847a903f677406b24a2fd90c010ea0d6683b842c
TrpFrog <dev@trpfrog.net>
2021-12-28 03:08:19 +0900

    Add store links on the top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,8 +80,50 @@ const Home: NextPage<PageProps> = ({myLinks, mutualLinks}: PageProps) => {
 
                 <Block title={'ストア'} h2icon={'otaku'} id={styles.sticker}>
                     <p>
-                        工事中
+                        つまみさんのスタンプ・グッズ<br/>
+                        好評発売中！
                     </p>
+                    <div className={styles.link_grid}>
+                        <div className={styles.link_block}>
+                            <a
+                                href="https://store.line.me/stickershop/product/4674940/ja"
+                                className="linkButton"
+                            >LINEスタンプ vol.1</a>
+                        </div>
+                        <div className={styles.link_block}>
+                            <Image
+                                src={'sticker_pr'}
+                                width={18}
+                                height={15}
+                                className={'richimage'}
+                                layout={'responsive'}
+                                objectFit={'contain'}
+                                alt={'つまみグッズの画像'}
+                            />
+                            <a
+                                href="https://store.line.me/stickershop/product/8879469/ja"
+                                className="linkButton"
+                                style={{marginTop: '10px'}}
+                            >LINEスタンプ vol.2</a>
+                        </div>
+                        <div className={styles.link_block}>
+                            <Image
+                                src={'goods'}
+                                width={100}
+                                height={70}
+                                layout={'responsive'}
+                                objectFit={'contain'}
+                                alt={'つまみグッズの画像'}
+                            />
+                            <a
+                                href="https://suzuri.jp/TrpFrog"
+                                className="linkButton"
+                            >つまみグッズ on SUZURI</a>
+                        </div>
+                    </div>
+                    {/*<p style="margin-top: 20px; text-align: center">*/}
+                    {/*    <img src="images/goods.webp" style="width: 100%;" alt="つまみグッズ">*/}
+                    {/*</p>*/}
                 </Block>
                 <Block title={'作ったアイコン'} h2icon={'evil'} id={styles.icons}>
                     <p>
