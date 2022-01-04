3c80846d193450bb13f5c969694e04a70bd45976
TrpFrog <dev@trpfrog.net>
2022-01-05 03:23:06 +0900

    Fix concentration lines drawn incorrectly

--- components/TrpFrogAnimation.tsx
+++ components/TrpFrogAnimation.tsx
@@ -3,9 +3,11 @@ import {motion} from "framer-motion";
 const TrpFrogAnimation = () => {
     return (
         <div id={'top-page-animation'}>
-            <motion.div id={'top-page-animation-lines'}
-                // animate={{ rotate: 360 }}
-                // transition={{ ease: "linear", duration: 2, repeat: Infinity }}
+            <motion.div
+                id={'top-page-animation-lines'}
+                initial={{ '--trpfrog-animation-start-degree': '0deg' } as any}
+                animate={{ '--trpfrog-animation-start-degree': '360deg' } as any}
+                transition={{ duration: 30, repeat: Infinity, ease: 'linear' }}
             />
             <div id={'top-page-trpfrog'}/>
         </div>
