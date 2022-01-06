8f2c1a76eff6e2efd3fa56227da49a03162c7c9a
TrpFrog <dev@trpfrog.net>
2022-01-06 21:26:51 +0900

    Show page title on the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,39 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import {useRouter} from "next/router";
 import {motion, MotionValue, useTransform, useViewportScroll} from "framer-motion";
+import {useEffect, useState} from "react";
 
-const normalTitle = (
-    <div id={'header-title'}>
-        <div id={'header-title-image'}/>
-        <h1>
-            <Link href="/">
-                <a>{process.env.title}</a>
-            </Link>
-        </h1>
-    </div>
-);
+const normalTitle = (showPageTitle: boolean, title: string) => {
+    const backToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    return (
+        <div id={'header-title'}>
+            <div id={'header-title-image'}/>
+            <h1>
+                {showPageTitle ? (
+                    <a onClick={backToTop} style={{cursor: 'pointer'}}>
+                        {
+                            process.browser
+                                ? title
+                                : process.env.title
+                        }
+                    </a>
+                ) : (
+                    <Link href="/">
+                        <a>
+                            {process.env.title}
+                        </a>
+                    </Link>
+                )}
+            </h1>
+        </div>
+    );
+};
 
 const topTitle = (iconBottom: MotionValue<number>, titleLeft: MotionValue<number>) => {
     return (
@@ -34,7 +55,7 @@ const topTitle = (iconBottom: MotionValue<number>, titleLeft: MotionValue<number
 };
 
 const Header = () => {
-    const router = useRouter();
+
     const { scrollY } = useViewportScroll();
     const icon = useTransform(scrollY, y => {
         return -80 + Math.min(1, Math.max(0, y - 200) / 175) * 79;
@@ -49,10 +70,35 @@ const Header = () => {
         }
     });
 
+    const [showPageTitle, setShowPageTitle] = useState(false);
+    useEffect(() => {
+        const heightToChangeTitle =
+            process.browser && window.innerWidth <= 800 ? 120 : 250;
+        const handleScroll = () => {
+            setShowPageTitle(window.scrollY > heightToChangeTitle);
+        };
+        // set initial state
+        handleScroll();
+        // register listener
+        window.addEventListener("scroll", handleScroll);
+        // clean up
+        return () => { window.removeEventListener("scroll", handleScroll); };
+    }, []);
+
+    const router = useRouter();
+    let pageTitle = process.browser
+        ? document.title.replace(' - ' + process.env.title, '')
+        : process.env.title as string;
+    if(router.pathname.startsWith('/blog')) {
+        pageTitle = 'つまみログ';
+    }
+
     return (
         <header>
             <div id="header-wrapper">
-                {router.pathname == '/' ? topTitle(icon, title) : normalTitle}
+                {router.pathname == '/'
+                    ? topTitle(icon, title)
+                    : normalTitle(showPageTitle, pageTitle)}
                 <nav>
                     <ul>
                         <li>
