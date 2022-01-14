f2211e1e469a92f97f525d74c4b0b6b736590144
TrpFrog <dev@trpfrog.net>
2022-01-14 20:11:33 +0900

    Make sure the header is hidden when scrolling down

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,13 +63,10 @@ const topTitle = (iconY: MotionValue<number>, titleX: MotionValue<number>) => {
 const Header = () => {
     const titleX = useMotionValue(-82);
     const iconY = useMotionValue(80);
-
+    const headerY = useMotionValue(0)
     const [showPageTitle, setShowPageTitle] = useState(false);
 
-    const handleScroll = (y: number, withAnimation: boolean = true) => {
-        const heightToChangeTitle =
-            process.browser && window.innerWidth <= 800 ? 120 : 250;
-
+    const animateTopPageHeader = (y: number, withAnimation: boolean) => {
         const animationHeight = 280;
         const isMobile = window.innerWidth < 800;
         const config: AnimationOptions<number> = {
@@ -77,7 +74,6 @@ const Header = () => {
             ease: 'linear'
         };
 
-        setShowPageTitle(y > heightToChangeTitle);
         if (y >= animationHeight) {
             animate(iconY, 0, config);
             animate(titleX, 4, config);
@@ -87,8 +83,41 @@ const Header = () => {
         }
     }
 
+    const changeHeaderH1 = (y: number) => {
+        const heightToChangeTitle =
+            process.browser && window.innerWidth <= 800 ? 120 : 250;
+        setShowPageTitle(y > heightToChangeTitle);
+    }
+
+    const setHeaderVisibility = (y: number) => {
+        const v = scrollY.getVelocity()
+        const shouldShowHeader = v < -1000 || y < 500;
+        const shouldHideHeader = !shouldShowHeader && v > 1000;
+
+        // if (process.browser && window.innerWidth <= 800) {
+        //     if (shouldShowHeader) {
+        //         headerY.set(0)
+        //     } else if (shouldHideHeader) {
+        //         headerY.set(-100)
+        //     }
+        // } else
+        {
+            if (shouldShowHeader) {
+                animate(headerY, 0, {duration: 0.05, ease: 'linear'})
+            } else if (shouldHideHeader) {
+                animate(headerY, -100, {duration: 0.1, ease: 'linear'})
+            }
+        }
+    }
+
+    const handleScroll = (y: number, withAnimation: boolean = true) => {
+        changeHeaderH1(y)
+        animateTopPageHeader(y, withAnimation)
+        setHeaderVisibility(y)
+    }
+
     const {scrollY} = useViewportScroll()
-    scrollY.onChange(y => handleScroll(y))
+    scrollY.onChange(handleScroll)
     useEffect(() => handleScroll(window.scrollY, false))
 
     const router = useRouter();
@@ -100,7 +129,9 @@ const Header = () => {
     }
 
     return (
-        <header>
+        <motion.header
+            style={{y: headerY}}
+        >
             <div id="header-wrapper">
                 {router.pathname == '/'
                     ? topTitle(iconY, titleX)
@@ -125,7 +156,7 @@ const Header = () => {
                     </ul>
                 </nav>
             </div>
-        </header>
+        </motion.header>
     );
 }
 
