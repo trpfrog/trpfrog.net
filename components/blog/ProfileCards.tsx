3117e5189afc6cbb2f23384a042f4aacd3067e4a
TrpFrog <dev@trpfrog.net>
2022-02-18 01:57:10 +0900

    Refactor ProfileCards.tsx

--- components/blog/ProfileCards.tsx
+++ components/blog/ProfileCards.tsx
@@ -3,18 +3,88 @@ import styles from "../../styles/blog/ProfileCards.module.scss";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 
-const ProfileCards = ({content}: {content: string}) => {
+export type ProfileData = {
+    name: string
+    club?: string
+    twitter?: string
+    description: string
+}
 
+type ProfileKey = 'name' | 'club' | 'twitter' | 'description'
+
+const CardFormat = ({personalDataList}: any) => (
+    <div className={styles.profile_card_grid}>
+        {personalDataList.map((personalData: any) => (
+            <div className={styles.profile_card} key={personalData.name}>
+                <div className={styles.header}>
+                    <div className={styles.club}>
+                        {personalData.club}
+                    </div>
+                    <div className={styles.twitter_id}>
+                        <a href={'https://twitter.com/' + personalData.twitter}>
+                            @{personalData.twitter}
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.name}>
+                    {personalData.name}
+                    <span style={{fontSize: '0.8em'}}>
+                        {personalData.name === 'つまみ' ? ' (筆者)' : 'さん'}
+                    </span>
+                </div>
+                <div className={styles.description}>
+                    <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                        {personalData.description}
+                    </ReactMarkdown>
+                </div>
+            </div>
+        ))}
+    </div>
+)
+
+const ListFormat = ({personalDataList}: any) => (
+    <ul>
+        {personalDataList.map((personalData: any) => (
+            <>
+                <li key={personalData.name + '-name'}>
+                    {personalData.name}
+                    {personalData.name === 'つまみ' ? ' (筆者)' : 'さん'}
+                </li>
+                <ul key={personalData.name + '-info'}>
+                    <li>{personalData.club}</li>
+                    <li>
+                        <a href={'https://twitter.com/' + personalData.twitter}>
+                            @{personalData.twitter}
+                        </a>
+                    </li>
+                    <li>
+                        <ReactMarkdown
+                            components={{ p: ({children}) => <>{children}</> }}
+                            rehypePlugins={[rehypeRaw]}
+                        >
+                            {personalData.description}
+                        </ReactMarkdown>
+                    </li>
+                </ul>
+            </>
+        ))}
+    </ul>
+)
+
+const ProfileCards = ({content}: {content: string}) => {
     const cards = content.split('---')
 
     const personalDataList = cards.map(card => {
         const personalData: { [key: string]: string } = {}
         const lines = card.trim().split('\n')
+
         for (const line of lines) {
-            const key = line.split(':')[0]
-            personalData[key] = line.split(':').slice(1).join(':').trim()
+            const divided = line.split(':').map(e => e.trim())
+            const key     = divided[0] as ProfileKey
+            const value   = divided.slice(1).join(':')
+            personalData[key] = value
         }
-        return personalData
+        return personalData as ProfileData
     })
 
     const [showProfileCards, setShowProfileCards] = useState(true)
@@ -36,60 +106,9 @@ const ProfileCards = ({content}: {content: string}) => {
                 </p>
             )}
             {showProfileCards ? (
-                <div className={styles.profile_card_grid}>
-                    {personalDataList.map(personalData => (
-                        <div className={styles.profile_card} key={personalData.name}>
-                            <div className={styles.header}>
-                                <div className={styles.club}>
-                                    {personalData.club}
-                                </div>
-                                <div className={styles.twitter_id}>
-                                    <a href={'https://twitter.com/' + personalData.twitter}>
-                                        @{personalData.twitter}
-                                    </a>
-                                </div>
-                            </div>
-                            <div className={styles.name}>
-                                {personalData.name}
-                                <span style={{fontSize: '0.8em'}}>
-                                        {personalData.name === 'つまみ' ? ' (筆者)' : 'さん'}
-                                    </span>
-                            </div>
-                            <div className={styles.description}>
-                                <ReactMarkdown rehypePlugins={[rehypeRaw]}>
-                                    {personalData.description}
-                                </ReactMarkdown>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <CardFormat personalDataList={personalDataList} />
             ) : (
-                <ul>
-                    {personalDataList.map(personalData => (
-                        <>
-                            <li key={personalData.name + '-name'}>
-                                {personalData.name}
-                                {personalData.name === 'つまみ' ? ' (筆者)' : 'さん'}
-                            </li>
-                            <ul key={personalData.name + '-info'}>
-                                <li>{personalData.club}</li>
-                                <li>
-                                    <a href={'https://twitter.com/' + personalData.twitter}>
-                                        @{personalData.twitter}
-                                    </a>
-                                </li>
-                                <li>
-                                    <ReactMarkdown
-                                        components={{ p: ({children}) => <>{children}</> }}
-                                        rehypePlugins={[rehypeRaw]}
-                                    >
-                                        {personalData.description}
-                                    </ReactMarkdown>
-                                </li>
-                            </ul>
-                        </>
-                    ))}
-                </ul>
+                <ListFormat personalDataList={personalDataList} />
             )}
         </>
     )
