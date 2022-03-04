f27c07b0f4cf98495b9ad57873263d98a09ab93f
TrpFrog <dev@trpfrog.net>
2022-03-04 21:51:36 +0900

    Refactor code

--- components/blog/ArticleCard.tsx
+++ components/blog/ArticleCard.tsx
@@ -1,37 +1,18 @@
-import React, {CSSProperties, FunctionComponent} from "react";
 import Link from "next/link";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCalendarDay, faClock} from "@fortawesome/free-solid-svg-icons";
-import {format, parseISO} from "date-fns";
 import {BlogPost} from "../../lib/blog/load";
 import styles from "../../styles/blog/ArticleCard.module.scss";
-import Title from "../Title";
 import Image from "next/image";
-import {formatReadTime} from "../../lib/blog/readTime";
 import {getPureCloudinaryPath} from "./BlogMarkdown";
 import dayjs from "dayjs";
 import Tag from "./Tag";
 
 type Props = {
     entry: BlogPost
-    showTitle?: boolean
-    showDescription?: boolean
-    showDate?: boolean
-    showUpdatedDate?: boolean
-    showTags?: boolean
-    thinPadding?: boolean
-    showBackground?: boolean
 }
 
 const ArticleCard = ({entry}: Props) => {
-
-    const thumbnailStyle: CSSProperties = entry.thumbnail ? {
-        backgroundImage: `url(${entry.thumbnail})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100%',
-        height: '100%'
-    } : {}
 
     return (
         <Link href={'/blog/entry/' + entry.slug}>
