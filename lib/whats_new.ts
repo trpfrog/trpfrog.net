2811c11bb9d3e9d01d0e8d58e6a97ececc9e91fb
TrpFrog <dev@trpfrog.net>
2022-03-19 01:18:55 +0900

    Make article updates are automatically apply on What's new

--- lib/whats_new.ts
+++ lib/whats_new.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import dayjs from "dayjs";
+import {getSortedPostsData} from "./blog/load";
 
 export type WhatsNewRecord = {
     type: 'page' | 'fix' | 'improve' | 'blog' | 'content',
@@ -10,5 +12,20 @@ export type WhatsNewRecord = {
 export const getWhatsNewRecords: () => Promise<WhatsNewRecord[]> = async () => {
     const jsonPath = path.join(process.cwd(), 'data', 'whats_new.json');
     const jsonText = fs.readFileSync(jsonPath, 'utf-8');
-    return JSON.parse(jsonText) as WhatsNewRecord[];
+    const records = JSON.parse(jsonText) as WhatsNewRecord[];
+
+    const blogData = await getSortedPostsData()
+
+    for (const post of blogData) {
+        records.push({
+            type: 'blog',
+            text: `記事「[${post.title}](https://trpfrog.net/blog/entry/${post.slug})」を公開しました！`,
+            date: dayjs(post.date).format('YYYY-MM-DD')
+        })
+    }
+
+    return records.sort((a, b) => {
+        if(a.date === b.date) return 0
+        return a.date < b.date ? 1 : -1
+    })
 }
