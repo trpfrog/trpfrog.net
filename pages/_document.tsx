d59ef7def44e7384b995f177263cafd732a8b33c
TrpFrog <dev@trpfrog.net>
2022-04-09 23:26:31 +0900

    Change directory of GoogleFonts component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Favicon from '../components/head/Favicon'
-import GoogleFonts from "../lib/googleFonts";
+import GoogleFonts from "../components/GoogleFonts";
 
 const MyDocument = () => {
     return (
