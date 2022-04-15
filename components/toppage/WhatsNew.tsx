f39b45e57021093d922de8ba109f905d01232637
TrpFrog <dev@trpfrog.net>
2022-04-16 02:53:58 +0900

    Fix broken layout in what's new on Firefox

--- components/toppage/WhatsNew.tsx
+++ components/toppage/WhatsNew.tsx
@@ -12,12 +12,15 @@ const WhatsNew = ({id, whatsNewRecords}: Props) => {
     return (
         <Block title={'最新情報'} h2icon={'robot'} id={id}>
             <div id={styles.whats_new_table}>
-                {whatsNewRecords.map(({text, date}) => (
-                    <div key={text} className={styles.whats_new_row}>
-                        <div className={styles.whats_new_date}>{date}</div>
-                        <div><ReactMarkdown>{text}</ReactMarkdown></div>
-                    </div>
-                ))}
+                {whatsNewRecords.map(({text, date}) => {
+                    const [y, m, d] = date.split('-')
+                    return (
+                        <div key={text} className={styles.whats_new_row}>
+                            <div className={styles.whats_new_date}>{y}-<br/>{m}-{d}</div>
+                            <div><ReactMarkdown>{text}</ReactMarkdown></div>
+                        </div>
+                    )
+                })}
             </div>
         </Block>
     )
