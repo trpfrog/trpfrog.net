2e7322a6a3ac3178879a6a790a58eeaec5335b50
TrpFrog <dev@trpfrog.net>
2022-05-04 17:12:05 +0900

    Refactor BlogImage and add taken-by attr

--- components/blog/BlogImage.tsx
+++ components/blog/BlogImage.tsx
@@ -1,9 +1,11 @@
 import React, {CSSProperties, useState} from "react";
-import styles from "../../styles/blog/blog.module.scss";
+import styles from "../../styles/blog/BlogImage.module.scss";
 import Image from "next/image";
 import Modal from "react-modal";
 import {getPureCloudinaryPath, parseInlineMarkdown} from "./BlogMarkdown";
 import {BlogImageData} from "../../lib/blog/imagePropsFetcher";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCamera} from "@fortawesome/free-solid-svg-icons";
 
 type BlogImageProps = {
     src: string,
@@ -26,7 +28,14 @@ const BlogImage = ({src, alt, imageData, style}: BlogImageProps) => {
 
     const srcPath = getPureCloudinaryPath(src)
     const blurPath = `https://res.cloudinary.com/trpfrog/image/upload/w_10${srcPath}`
-    const caption = imageData.caption ?? ''
+
+    let caption = imageData.caption ?? ''
+    let takenBy: string | undefined
+
+    const takenByIdentifier = 'taken-by:'
+    if (caption.includes(takenByIdentifier)) {
+        [caption, takenBy] = caption.split(takenByIdentifier).map(e => e.trim())
+    }
 
     let width = imageData.size?.width ?? 1200
     let height = imageData.size?.height ?? 900
@@ -60,11 +69,19 @@ const BlogImage = ({src, alt, imageData, style}: BlogImageProps) => {
     const [modalState, setModalState] = useState(false)
     return (
         <>
-            <figure className={styles.blog_img_wrapper} style={style}>
+            <figure className={styles.img_wrapper} style={style}>
+                {takenBy &&
+                    <div style={{textAlign: 'right'}}>
+                        <small className={styles.taken_by}>
+                            <FontAwesomeIcon icon={faCamera}/>{' '}
+                            撮影: {parseInlineMarkdown(takenBy)}
+                        </small>
+                    </div>
+                }
                 <Image
                     src={srcPath}
                     alt={alt || src}
-                    className={`rich_image ${styles.blog_img}`}
+                    className={`rich_image ${styles.image}`}
                     width={width}
                     height={height}
                     quality={50}
@@ -73,8 +90,8 @@ const BlogImage = ({src, alt, imageData, style}: BlogImageProps) => {
                     objectFit="contain"
                     onClick={() => setModalState(true)}
                 />
-                {caption != '' &&
-                    <figcaption className={styles.blog_img_caption}>
+                {caption &&
+                    <figcaption className={styles.caption}>
                         {parseInlineMarkdown(caption)}
                     </figcaption>
                 }
