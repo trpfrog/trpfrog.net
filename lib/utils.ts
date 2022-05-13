d24c6579b66cd789978d41166d9ff09a28c803f4
TrpFrog <dev@trpfrog.net>
2022-05-13 22:01:46 +0900

    Fix clamp cannot use

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 
-namespace Util {
-    export const clamp = (x: number, min: number, max: number): number => {
+const Util =  {
+    clamp: (x: number, min: number, max: number): number => {
         assert(min <= max)
         if (x < min) return min
         if (x > max) return max
