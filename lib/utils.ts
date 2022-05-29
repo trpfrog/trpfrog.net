0757e8e2be1e9c0aad10985d5610af90303ece4b
TrpFrog <dev@trpfrog.net>
2022-05-29 19:12:52 +0900

    Add getTextWidth in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import assert from "assert";
+import {useEffect, useState} from "react";
 
 const Util =  {
     clamp: (x: number, min: number, max: number): number => {
@@ -6,6 +7,14 @@ const Util =  {
         if (x < min) return min
         if (x > max) return max
         return x
+    },
+
+    getTextWidth: (text: string, font: string) => {
+        if (typeof window === 'undefined') return 0;
+        const canvas = document.createElement('canvas') as HTMLCanvasElement;
+        const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        context.font = font || getComputedStyle(document.body).font;
+        return context.measureText(text).width;
     }
 }
 
