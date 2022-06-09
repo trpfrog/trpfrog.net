8102efc6de41088a763bc0d182b08b840d2dcdbe
TrpFrog <dev@trpfrog.net>
2022-06-09 22:08:05 +0900

    Add Horizontal-scroll component

--- components/blog/BlogMarkdown.tsx
+++ components/blog/BlogMarkdown.tsx
@@ -249,6 +249,24 @@ const myMarkdownClasses: MarkdownFunctionType = {
         )
     },
 
+    'Horizontal-scroll': (content, entry, imageSize) => {
+        const contents = content.split('\n\n').filter(e => e.trim() !== '')
+        return (
+            <div style={{display: 'flex', overflowX: 'scroll', gap: '1em', whiteSpace: 'nowrap'}}>
+                {contents.map((e, idx) => (
+                    <div key={'horizontal-element-key-' + idx}
+                         style={{display: 'inline-block', whiteSpace: 'initial'}}>
+                        <ArticleRenderer
+                            toRender={e}
+                            entry={entry}
+                            imageSize={imageSize}
+                        />
+                    </div>
+                ))}
+            </div>
+        )
+    },
+
     Conversation: content => {
         const elements: React.ReactNode[] = []
 
