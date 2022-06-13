0fdb5348b5aec2fe3a75b1043efd79b9ca27fa48
TrpFrog <dev@trpfrog.net>
2022-06-14 03:47:09 +0900

    Add HeaderFollowSticky Component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,35 @@
 import Link from 'next/link'
 import {useRouter} from "next/router";
 import {animate, motion, useMotionValue, useViewportScroll} from "framer-motion";
-import React from "react";
+import React, {useState} from "react";
 import {NormalTitle} from "./NormalTitle";
 import {TopTitle} from "./TopTitle";
 import styles from "../../styles/common/Header.module.scss";
+
+export const HeaderFollowSticky = (props: {children: React.ReactNode, top: string | number}) => {
+
+    const {scrollY} = useViewportScroll()
+    const [headerTop, setHeaderTop] = useState(`calc(var(--header-height) + ${props.top})`)
+
+    const handleScroll = (y: number) => {
+        const v = scrollY.getVelocity()
+        const shouldShowHeader = v < -1000 || y < 500;
+        const shouldHideHeader = !shouldShowHeader && v > 1000;
+
+        if (shouldShowHeader) {
+            setHeaderTop(`calc(var(--header-height) + ${props.top})`)
+        } else if (shouldHideHeader) {
+            setHeaderTop(`${props.top}`)
+        }
+    }
+    scrollY.onChange(handleScroll)
+
+    return (
+        <div style={{transition: '0.1s', position:'sticky', top: headerTop}}>
+            {props.children}
+        </div>
+    )
+}
 
 const Header: React.FC = ({children}) => {
 
