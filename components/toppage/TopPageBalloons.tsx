74aa8951c5ebb5d1884b16ec9416838fff92ace4
TrpFrog <dev@trpfrog.net>
2022-11-03 11:23:15 +0900

    Update TopPageBalloons.tsx

--- components/toppage/TopPageBalloons.tsx
+++ components/toppage/TopPageBalloons.tsx
@@ -1,7 +1,9 @@
 import Block from "../Block";
 import styles from "../../styles/top-page/main.module.scss";
-import {BalloonBox} from "../../pages/balloon";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
+const Balloon = dynamic(() => import('../../components/Balloon'), {ssr: false});
 
 type Props = {
   id?: string
@@ -11,7 +13,7 @@ const TopPageBalloons = ({id}: Props) => {
   return (
     <Block title={'風船コーナー'} h2icon={'ice'} id={id}>
       <div id={styles.top_balloon_grid}>
-        {Array.from(Array(7), (v, k) => <BalloonBox key={k} width={'100%'} height={'100%'}/>)}
+        {Array.from(Array(7), (v, k) => <Balloon key={k} width={'100%'} height={'100%'}/>)}
       </div>
       <p>
         <Link href={'/balloon'} className={'linkButton'}>
