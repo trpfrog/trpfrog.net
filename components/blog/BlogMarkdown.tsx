92cd8ebdfe3e0dd0edcfaa8d73777363745ba544
TrpFrog <dev@trpfrog.net>
2022-12-08 20:55:41 +0900

    Add a blog component Dangerously-set-inner-html

--- components/blog/BlogMarkdown.tsx
+++ components/blog/BlogMarkdown.tsx
@@ -155,6 +155,10 @@ const myMarkdownClasses: MarkdownFunctionType = {
     </>
   ),
 
+  'Dangerously-set-inner-html': content => (
+    <div dangerouslySetInnerHTML={{__html: content}}/>
+  ),
+
 
 }
 
