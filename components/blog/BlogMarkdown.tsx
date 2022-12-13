0fdd7ee5eef51adb20346a27252a45ba9c4ca0f2
TrpFrog <dev@trpfrog.net>
2022-12-14 01:14:26 +0900

    Fix incorrect aspect ratio of images on articles

--- components/blog/BlogMarkdown.tsx
+++ components/blog/BlogMarkdown.tsx
@@ -222,8 +222,8 @@ const getFormatCodeComponent = (entry?: BlogPost, imageSize?: { [path: string]: 
 export const getPureCloudinaryPath = (path: string) => {
   const cloudinaryUrl = 'https:\/\/res.cloudinary.com\/trpfrog'
   return (path ?? '')
-    .replace(/\/image\/upload/, '')
     .replace(/\/image\/upload\/v[0-9]+/, '')
+    .replace(/\/image\/upload/, '')
     .replace(cloudinaryUrl, '')
     .split('.')[0] // remove extension
 }
