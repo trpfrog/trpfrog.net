3a0ed4b56fe5e8d87ca95975e4599727aa8ecbe6
TrpFrog <dev@trpfrog.net>
2023-03-21 22:06:31 +0900

    Migrate music page to appdir

---
+++ app/music/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: 'つまみのうた',
+  description: 'ねぎ一世さん作曲「つまみのうた」の歌詞',
+}
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({children}: Props) {
+  return (
+    <div id="main_wrapper">
+      {children}
+    </div>
+  )
+}
