fea0a910f4f826703d18067f7377995920fc0577
TrpFrog <dev@trpfrog.net>
2023-05-03 05:04:45 +0900

    Add anchor link on the search box

--- app/tweets/SearchForm.tsx
+++ app/tweets/SearchForm.tsx
@@ -3,7 +3,7 @@ import styles from './SearchForm.module.scss'
 
 export default function SearchForm(props: {defaultValue?: string}) {
   return (
-    <form action="/tweets" method="get" className={styles.searchForm}>
+    <form action="/tweets#tweets" method="get" className={styles.searchForm}>
       <input type="text" name="q" placeholder="ツイート検索" defaultValue={props.defaultValue} />
       <input type="submit" value="検索" />
     </form>
