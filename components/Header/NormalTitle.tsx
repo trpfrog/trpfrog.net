495966dd3d967ac033ec40cbb3ae01559037f783
TrpFrog <dev@trpfrog.net>
2023-06-30 02:21:57 +0900

    Add missing files
    https://vercel.com/guides/how-do-i-resolve-a-module-not-found-error

---
+++ components/Header/NormalTitle.tsx
@@ -0,0 +1,61 @@
+import {useEffect, useState} from "react";
+import {useScroll} from "framer-motion";
+import {usePathname} from "next/navigation";
+import Link from "next/link";
+import styles from "./index.module.scss";
+
+const TitleWithPageName = () => {
+  const pathname = usePathname() ?? '/'
+
+  let siteTitle = pathname.startsWith('/blog/')
+    ? 'つまみログ'
+    : process.env.title as string
+
+  const [pageTitle, setPageTitle] = useState('')
+  useEffect(() => {
+    setPageTitle(document?.title.split(' - ')[0] ?? '')
+  }, [])
+
+  return (
+    <div className={styles.on_subtitle_showed}>
+      {siteTitle}
+      {pageTitle ? (
+        <>
+          <br/>
+          <div id={styles.subtitle}>{pageTitle}</div>
+        </>
+      ) : null}
+    </div>
+  )
+}
+
+export const NormalTitle = () => {
+  const [showPageTitle, setShowPageTitle] = useState(false);
+
+  const [heightToChangeTitle, setHeightToChangeTitle] = useState(250)
+  useEffect(() => setHeightToChangeTitle(window.innerWidth <= 800 ? 120 : 250), [])
+
+  const {scrollY} = useScroll()
+  scrollY.on("change", (y: number) => {
+    setShowPageTitle(y > heightToChangeTitle);
+  })
+
+  return (
+    <div id={styles.site_logo}>
+      <div id={styles.trpfrog_icon}/>
+      <div id={styles.site_name_wrapper}>
+        <h1 id={styles.site_name}>
+          {showPageTitle ? (
+            <Link href="/" style={{cursor: 'pointer'}}>
+              <TitleWithPageName/>
+            </Link>
+          ) : (
+            <Link href="/">
+              {process.env.title}
+            </Link>
+          )}
+        </h1>
+      </div>
+    </div>
+  );
+};
