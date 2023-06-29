f86d1499eb8319b0d2e3da7dd2f5298453992690
TrpFrog <dev@trpfrog.net>
2023-06-30 01:07:55 +0900

    Refactor directory structure of LoadingBlock

---
+++ components/LoadingBlock/index.tsx
@@ -0,0 +1,32 @@
+import styles from "./index.module.scss";
+import Block from "../Block";
+import {CSSProperties} from "react";
+
+type Props = {
+  isFullHeight?: boolean
+  style?: CSSProperties
+}
+
+export default function LoadingBlock({
+  isFullHeight = false,
+  ...props
+}: Props) {
+  return (
+    <Block className={isFullHeight ? styles.fullscreen_block : ''} style={props.style}>
+      <div className={styles.wrapper}>
+        <span className={styles.text}>
+          <span>L</span>
+          <span>o</span>
+          <span>a</span>
+          <span>d</span>
+          <span>i</span>
+          <span>n</span>
+          <span>g</span>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </span>
+      </div>
+    </Block>
+  )
+}
