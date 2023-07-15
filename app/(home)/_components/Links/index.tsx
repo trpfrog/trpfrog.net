da1183fbfa9c6f2fa068165e8a1da80ddac0a232
TrpFrog <dev@trpfrog.net>
2023-07-15 22:30:37 +0900

    Refactor MyLinks

--- app/(home)/_components/Links/index.tsx
+++ app/(home)/_components/Links/index.tsx
@@ -1,19 +1,23 @@
 import Block from "@/components/Block";
 import styles from "@/app/(home)/style.module.scss";
 import Link from "next/link";
-import {getMyLinkRecords, MyLinkRecord} from "@/lib/MyLinks";
+import links from './links.json';
+
+export type MyLinkRecord = {
+  url: string,
+  siteName: string,
+  description: string
+}
 
 type Props = {
   id?: string
 }
 
 export default async function Links({id}: Props) {
-  const myLinks: MyLinkRecord[] = await getMyLinkRecords()
-
   return (
     <Block title={'リンク集'} h2icon={'robot'} id={styles.links}>
       <div className={styles.link_grid}>
-        {myLinks.map(({url, siteName, description}) => (
+        {(links as MyLinkRecord[]).map(({url, siteName, description}) => (
           <div key={siteName} className={styles.link_block}>
             <p style={{textAlign: "center"}}>
               <a href={url} className="linkButton">
