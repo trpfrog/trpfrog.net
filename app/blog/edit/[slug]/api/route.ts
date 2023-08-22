86c8f4b4c4c1f753878ec65309f14409485e5a60
TrpFrog <dev@trpfrog.net>
2023-08-23 00:30:38 +0900

    Add guard statements in edit api

--- app/blog/edit/[slug]/api/route.ts
+++ app/blog/edit/[slug]/api/route.ts
@@ -1,11 +1,17 @@
 import fs from 'fs'
 import path from 'path'
+import {getAllPostSlugs} from "@blog/_lib/load";
 
 export async function GET(req: Request) {
   const slug = req.headers.get('x-blog-slug')
   if (!slug) {
     return new Response('Missing x-blog-slug header', {status: 400})
   }
+
+  if (!(await getAllPostSlugs()).includes(slug)) {
+    return new Response('Not found', {status: 404})
+  }
+
   const md = fs.readFileSync(path.join(process.cwd(), 'posts', slug + '.md'), 'utf-8')
 
   try {
@@ -16,7 +22,18 @@ export async function GET(req: Request) {
 }
 
 export async function POST(req: Request) {
+  if (process.env.NODE_ENV !== 'development') {
+    return new Response('Forbidden', {status: 403})
+  }
+
   const slug = req.headers.get('x-blog-slug')
+  if (!slug) {
+    return new Response('Missing x-blog-slug header', {status: 400})
+  }
+  if (!(await getAllPostSlugs()).includes(slug)) {
+    return new Response('Not found', {status: 404})
+  }
+
   const body = await req.text()
 
   try {
