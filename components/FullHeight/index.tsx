0623b960aecf500b9f8bfee0a1cbac05788d5cac
TrpFrog <dev@trpfrog.net>
2023-08-25 23:26:04 +0900

    Add FullHeight Component

---
+++ components/FullHeight/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+export default function FullHeight(props: React.ComponentProps<'div'>) {
+  const {className = '', ...rest} = props;
+  return <div className={`${styles.fullscreen_block} ${className}`} {...rest}/>;
+}
