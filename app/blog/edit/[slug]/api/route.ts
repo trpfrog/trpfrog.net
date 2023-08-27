5eca67ffe21c07ef50b695a041c42f62320f0dd0
TrpFrog <dev@trpfrog.net>
2023-08-27 23:12:44 +0900

    Add save API

--- app/blog/edit/[slug]/api/route.ts
+++ app/blog/edit/[slug]/api/route.ts
@@ -21,25 +21,3 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(req: Request) {
-  if (process.env.NODE_ENV !== 'development') {
-    return new Response('Forbidden', {status: 403})
-  }
-
-  const slug = req.headers.get('x-blog-slug')
-  if (!slug) {
-    return new Response('Missing x-blog-slug header', {status: 400})
-  }
-  if (!(await getAllPostSlugs()).includes(slug)) {
-    return new Response('Not found', {status: 404})
-  }
-
-  const body = await req.text()
-
-  try {
-    fs.writeFileSync(path.join(process.cwd(), 'posts', slug + '.md'), body)
-    return new Response('Saved')
-  } catch (e) {
-    return new Response('Not found', {status: 404})
-  }
-}
