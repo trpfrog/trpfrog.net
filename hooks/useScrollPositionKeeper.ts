dd233c08efe15c101e6f5d2696aa5b32742a7b0f
TrpFrog <dev@trpfrog.net>
2023-08-29 23:22:36 +0900

    Support useScrollPositionKeeper with no ref

--- hooks/useScrollPositionKeeper.ts
+++ hooks/useScrollPositionKeeper.ts
@@ -1,20 +1,23 @@
 import React from "react";
 
-export default function useScrollPositionKeeper(ref: React.RefObject<HTMLElement>) {
+export default function useScrollPositionKeeper(ref?: React.RefObject<HTMLElement>) {
   const scrollPosition = React.useRef(0);
 
   // restore scroll position
   React.useEffect(() => {
-    ref.current?.scrollTo(0, scrollPosition.current);
+    const element = ref ? ref.current : typeof window !== "undefined" ? window : undefined;
+    element?.scrollTo(0, scrollPosition.current);
   })
 
   // register scroll event
   React.useEffect(() => {
-    const element = ref.current;
+    const element = ref ? ref.current : typeof window !== "undefined" ? window : undefined;
     if (!element) return;
 
     const handleScroll = () => {
-      scrollPosition.current = element.scrollTop;
+      scrollPosition.current = 'scrollTop' in element
+        ? element.scrollTop
+        : (element ? window.scrollY : 0)
     };
 
     element.addEventListener("scroll", handleScroll);
