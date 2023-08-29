0e8152ef047e6297fc8aabca78a9bc9cbfcd2bf6
TrpFrog <dev@trpfrog.net>
2023-08-29 21:55:10 +0900

    Change design of EditorForm

--- app/blog/edit/[slug]/EditorForm.tsx
+++ app/blog/edit/[slug]/EditorForm.tsx
@@ -4,12 +4,24 @@ import {useForm} from "react-hook-form";
 import {BlogFrontMatter, blogFrontMatterSchema, buildBlogPost} from "@blog/_lib/blogPost";
 import {getTypedEntries, getTypedKeys} from "@/lib/utils";
 import matter from "gray-matter";
-import {useCallback, useEffect, useMemo} from "react";
+import React, {useCallback, useEffect, useMemo} from "react";
+import styles from "./page.module.scss"
 
 type Props = {
   setPost: ((value: string) => void)
   rawMarkdown: string
   markAsUnsaved: () => void
+}
+
+function FormItem(props: React.PropsWithChildren<{htmlFor?: string, label: string}>) {
+  return (
+    <div className={styles.form_item}>
+      <label htmlFor={props.htmlFor}>{props.label}</label>
+      <div>
+        {props.children}
+      </div>
+    </div>
+  )
 }
 
 export default function EditorForm(props: Props) {
@@ -37,35 +49,28 @@ export default function EditorForm(props: Props) {
 
   return (
     <>
-      <form onChange={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="title">Title</label>
+      <form onChange={handleSubmit(onSubmit)} className={styles.editor_form}>
+        <FormItem htmlFor="title" label="Title">
           <input {...register("title")} />
-        </div>
-        <div>
-          <label htmlFor="date">Date</label>
+        </FormItem>
+        <FormItem htmlFor="date" label="Date">
           <input type="date" {...register("date")} />
-        </div>
-        <div>
-          <label htmlFor="updated">Updated</label>
+        </FormItem>
+        <FormItem htmlFor="updated" label="Updated">
           <input type="date" {...register("updated")} />
-        </div>
-        <div>
-          <label htmlFor="held">Held</label>
+        </FormItem>
+        <FormItem htmlFor="held" label="Held">
           <input type="date" {...register("held")} />
-        </div>
-        <div>
-          <label htmlFor="tags">Tags</label>
+        </FormItem>
+        <FormItem htmlFor="tags" label="Tags">
           <input {...register("tags")} />
-        </div>
-        <div>
-          <label htmlFor="description">Description</label>
+        </FormItem>
+        <FormItem htmlFor="description" label="Description">
           <input {...register("description")} />
-        </div>
-        <div>
-          <label htmlFor="thumbnail">Thumbnail</label>
+        </FormItem>
+        <FormItem htmlFor="thumbnail" label="Thumbnail">
           <input {...register("thumbnail")} />
-        </div>
+        </FormItem>
       </form>
     </>
   );
