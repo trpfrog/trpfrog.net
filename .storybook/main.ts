eb4d5de8aee511fc6064f96b49577dac755de6f7
TrpFrog <dev@trpfrog.net>
2023-09-02 23:39:52 +0900

    Install storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,37 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+
+  // Support import alias
+  webpackFinal: async config => {
+    if (!config.resolve) {
+      return config
+    }
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, '../src'),
+    ]
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    }
+
+    return config
+  },
+}
+export default config
