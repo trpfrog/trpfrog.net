2d732e8203405011694f5b75927d3be78e426da8
TrpFrog <dev@trpfrog.net>
2023-09-04 01:37:29 +0900

    Add Button component

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,51 @@
+import FloatingCircleButton from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+const meta: Meta<typeof FloatingCircleButton> = {
+  component: FloatingCircleButton,
+}
+
+export default meta
+
+type Story = StoryObj<typeof FloatingCircleButton>
+
+export const NextLink: Story = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    href: '/blog',
+    children: 'Blog',
+  },
+}
+
+export const ExternalLink: Story = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    externalLink: true,
+    href: 'https://example.com',
+    children: 'Jump to example.com',
+  },
+}
+
+export const Button: Story = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    onClick: action('Clicked'),
+    children: 'Button',
+  },
+}
+
+export const Other: Story = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    children: 'Nothing happens',
+  },
+}
---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styles from './index.module.scss'
+import Link from 'next/link'
+
+type LinkProps = Omit<Parameters<typeof Link>[0], 'href'> & {
+  href: `/${string}`
+}
+type ButtonProps = React.ComponentProps<'button'>
+type DivProps = React.ComponentProps<'div'>
+type AProps = React.ComponentProps<'a'>
+
+type Props =
+  | DivProps
+  | ({ onClick: ButtonProps['onClick'] } & ButtonProps)
+  | ({ externalLink?: false } & LinkProps)
+  | ({ externalLink: true } & AProps)
+
+type TagType = 'div' | 'button' | 'a' | 'Link'
+
+function getType<P extends Props>(props: P): TagType {
+  if ('onClick' in props) {
+    return 'button'
+  }
+  if ('externalLink' in props && props.externalLink) {
+    return 'a'
+  }
+  if ('href' in props) {
+    return 'Link'
+  }
+  return 'div'
+}
+
+function Wrapper<T extends TagType>(
+  props: { tag: T } & (T extends infer U extends 'a' | 'div' | 'button'
+    ? React.ComponentProps<U>
+    : LinkProps),
+) {
+  const { tag, ...rest } = props
+
+  switch (tag) {
+    case 'Link':
+      // @ts-ignore
+      return <Link {...(rest as LinkProps)} />
+    case 'a':
+      return (
+        <a {...(rest as AProps)} target="_blank" rel="noreferrer noopener" />
+      )
+    case 'button':
+      return <button {...(rest as ButtonProps)} />
+    case 'div':
+      return <div {...(rest as DivProps)} />
+  }
+}
+
+export default function Button(props: Props) {
+  const tag = getType(props)
+  const { className = '', ...rest } = props
+  return (
+    <Wrapper {...rest} tag={tag} className={`${styles.button} ${className}`} />
+  )
+}
