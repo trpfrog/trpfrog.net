3a9f03495da35d6cad6a349025a157b95e8f87f5
TrpFrog <dev@trpfrog.net>
2023-09-04 17:26:24 +0900

    Add WaveText component

---
+++ src/components/atoms/WaveText/index.stories.tsx
@@ -0,0 +1,19 @@
+import WaveText from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof WaveText> = {
+  component: WaveText,
+}
+
+export default meta
+
+type Story = StoryObj<typeof WaveText>
+
+export const Primary: Story = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    children: 'Waving Text',
+  },
+}
---
+++ src/components/atoms/WaveText/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+type Props<T extends string> = Omit<
+  React.ComponentProps<'span'>,
+  'children'
+> & {
+  children: T
+  durationPerPeriodMs?: number
+  intervalMs?: number
+}
+
+export default function WaveText<T extends string>(props: Props<T>) {
+  if (props.children.length === 0) return <></>
+
+  const { durationPerPeriodMs = 1500, intervalMs = 700 } = props
+  const delayMs = props.children.length
+    ? (durationPerPeriodMs - intervalMs) / props.children.length
+    : 0
+
+  const { className = '', ...rest } = props
+
+  return (
+    <span className={`${styles.wave_text} ${className}`} {...rest}>
+      {props.children.split('').map((char, i) => (
+        <span
+          key={i}
+          className={styles.char}
+          style={{
+            animationDuration: `${durationPerPeriodMs}ms`,
+            animationDelay: `${(i + 1) * delayMs}ms`,
+          }}
+        >
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
