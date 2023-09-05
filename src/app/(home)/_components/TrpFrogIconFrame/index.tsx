d3456b1a4d42db69327065bfef0ab41a272848c1
TrpFrog <dev@trpfrog.net>
2023-09-06 03:23:32 +0900

    Improve IconFrame using SWR

--- src/app/(home)/_components/TrpFrogIconFrame/index.tsx
+++ src/app/(home)/_components/TrpFrogIconFrame/index.tsx
@@ -1,108 +1,15 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 
-import WaveText from '@/components/atoms/WaveText'
+import { IconFrame } from '@/app/(home)/_components/TrpFrogIconFrame/IconFrame'
+
 import Block from '@/components/molecules/Block'
-
-import { generateRandomPrompt, generateTrpFrogImage } from '@/lib/randomTrpFrog'
-
-import styles from './index.module.scss'
-
-const AIIconFrame = React.memo(async function AIIconFrame(props: {
-  imageBase64: string
-  alt: string
-  size?: number | string
-}) {
-  return (
-    <img
-      style={{ width: props.size }}
-      className={styles.picture}
-      src={`data:image/png;base64,${props.imageBase64}`}
-      alt={`Auto generated image by AI: ${props.alt}`}
-    />
-  )
-})
-
-function DiffusionWaitingFallBack() {
-  return (
-    <figure style={{ margin: '20px 0' }}>
-      <div className={styles.loading_picture}>
-        <WaveText>Generating...</WaveText>
-      </div>
-      <figcaption className={styles.caption_wrapper}>
-        <div className={`${styles.caption_en} ${styles.waiting}`} />
-        <div className={`${styles.caption_ja} ${styles.waiting}`} />
-      </figcaption>
-    </figure>
-  )
-}
-
-function DiffusionErrorFallback() {
-  return (
-    <figure style={{ margin: '20px 0' }}>
-      <div className={styles.error_picture}>
-        <span>Error occurred</span>
-      </div>
-      <figcaption className={styles.caption_wrapper}>
-        <p>
-          エラーが発生しました。
-          <br />
-          時間をあけて再度お試しください。
-        </p>
-      </figcaption>
-    </figure>
-  )
-}
-
-async function IconFrame() {
-  if (process.env.NODE_ENV === 'development') {
-    return <DiffusionWaitingFallBack />
-  }
-
-  let base64 = ''
-  let prompt = ''
-  let translated = ''
-  const numberOfRetries = 3
-  for (const _ of Array(numberOfRetries)) {
-    try {
-      const promptRes = await generateRandomPrompt()
-      prompt = promptRes.prompt
-      translated = promptRes.translated
-      const result = await generateTrpFrogImage(prompt)
-      if (!result.success) {
-        continue
-      }
-      base64 = result.base64
-    } catch (e) {
-      console.error(e)
-      continue
-    }
-    break
-  }
-
-  if (!base64) {
-    return <DiffusionErrorFallback />
-  }
-
-  return (
-    <figure style={{ margin: '20px 0' }}>
-      <AIIconFrame imageBase64={base64} alt={prompt} />
-      <figcaption className={styles.caption_wrapper}>
-        <div className={styles.caption_en}>{prompt}</div>
-        <div className={styles.caption_ja}>{translated}</div>
-      </figcaption>
-    </figure>
-  )
-}
 
 type Props = React.ComponentProps<'div'>
 
 export default function TrpFrogIconFrame(props: Props) {
   return (
     <Block title={'TrpFrog Diffusion'} h2icon={'robot'} {...props}>
-      <Suspense fallback={<DiffusionWaitingFallBack />}>
-        <IconFrame />
-      </Suspense>
-
+      <IconFrame />
       <hr />
       <small style={{ fontSize: '0.6em' }}>
         <div>この画像は一定時間ごとに自動生成されます。</div>
---
+++ src/app/api/trpfrog-diffusion/route.ts
@@ -0,0 +1,109 @@
+import { NextResponse } from 'next/server'
+
+import { generateRandomPrompt, generateTrpFrogImage } from '@/lib/randomTrpFrog'
+
+export type TrpFrogImageGenerationResult = {
+  generatedTime: number
+  prompt: string
+  translated: string
+  base64: string
+}
+
+let cache: TrpFrogImageGenerationResult | null = null
+
+async function generateNew(options?: { numberOfRetries?: number }) {
+  let base64 = ''
+  let prompt = ''
+  let translated = ''
+  const numberOfRetries = options?.numberOfRetries ?? 1
+  for (const _ of Array.from(Array(numberOfRetries))) {
+    try {
+      const promptRes = await generateRandomPrompt()
+      prompt = promptRes.prompt
+      translated = promptRes.translated
+      const result = await generateTrpFrogImage(prompt)
+      if (!result.success) {
+        continue
+      }
+      base64 = result.base64
+    } catch (e) {
+      console.error(e)
+      continue
+    }
+    break
+  }
+
+  if (!base64) {
+    throw new Error('Failed to generate image')
+  }
+  const generated: TrpFrogImageGenerationResult = {
+    generatedTime: Date.now(),
+    prompt,
+    translated,
+    base64,
+  }
+  return generated
+}
+
+let updating = false
+
+export async function GET() {
+  if (process.env.NODE_ENV === 'development') {
+    const image = await fetch(
+      'https://res.cloudinary.com/trpfrog/icons_gallery/29',
+    ).then(res => res.blob())
+    return NextResponse.json({
+      generatedTime: Date.now(),
+      prompt: 'a photo of trpfrog',
+      translated: 'つまみさんの画像',
+      base64: Buffer.from(await image.arrayBuffer()).toString('base64'),
+      updating,
+    })
+  }
+
+  // if cache is not available but updating is true, wait for generating new one
+  while (!cache && updating) {
+    // sleep 10 seconds
+    await new Promise(resolve => setTimeout(resolve, 10000))
+  }
+
+  if (!cache) {
+    try {
+      updating = true
+      const generated = await generateNew({
+        numberOfRetries: 3,
+      })
+      cache = generated
+      return NextResponse.json({ ...generated, updating })
+    } catch (e) {
+      return NextResponse.json(
+        {
+          error: 'Internal Server Error, please try again later.',
+        },
+        {
+          status: 500,
+        },
+      )
+    } finally {
+      updating = false
+    }
+  }
+
+  const cacheHour = 3
+  if (Date.now() - cache.generatedTime > 1000 * 60 * 60 * cacheHour) {
+    if (!updating) {
+      // generate new one **in background**
+      updating = true
+      generateNew()
+        .then(generated => {
+          cache = generated
+        })
+        .catch(console.error)
+        .finally(() => {
+          updating = false
+        })
+    }
+  }
+
+  return NextResponse.json({ ...cache, updating })
+}
