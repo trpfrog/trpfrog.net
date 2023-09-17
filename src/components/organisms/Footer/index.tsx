97abdff8951b7bffdfffbb0a1d2dd56b8491e54f
TrpFrog <dev@trpfrog.net>
2023-09-18 00:59:34 +0900

    Migrate layout stylesheets to CSS modules

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,18 +1,20 @@
 import Link from 'next/link'
 
+import MainWrapper from '@/components/atoms/MainWrapper'
+
 import styles from './index.module.scss'
 
 const Footer = () => {
   return (
     <footer id={styles.footer}>
-      <div id={styles.inner_footer}>
+      <MainWrapper id={styles.inner_footer}>
         <p id={styles.copyright}>&copy; 2019-2023 つまみ</p>
         <p>
           <Link href={'/legal'} className={styles.footer_button}>
             Legal
           </Link>
         </p>
-      </div>
+      </MainWrapper>
     </footer>
   )
 }
