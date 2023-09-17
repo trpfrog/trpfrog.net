f680d2307e4201c3f0fbe9a19e9cf756679f150f
TrpFrog <dev@trpfrog.net>
2023-09-18 00:59:52 +0900

    Add AnglePicker to toppage

--- src/app/(home)/_components/TrpFrogAnimation/index.tsx
+++ src/app/(home)/_components/TrpFrogAnimation/index.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 
-import { motion } from 'framer-motion'
-import { parseCookies, setCookie } from 'nookies'
+import { Tooltip } from 'react-tooltip'
+
+import AnglePicker from '@/components/atoms/AnglePicker'
 
 import styles from './index.module.scss'
 
@@ -13,47 +14,57 @@ type Props = {
 }
 
 export default function TrpFrogAnimation({ children, id }: Props) {
-  const cookies = parseCookies()
-  const cookieName = 'doNotPlayTopPageAnimation'
-  const doAnimation = cookies[cookieName] !== 'true'
-
-  useEffect(() => {
-    setCookie(null, cookieName, 'true', {
-      maxAge: 60 * 60 * 24 * 14,
-      path: '/',
-    })
-  }, [])
-
+  const ref = React.useRef<HTMLDivElement>(null)
   return (
     <>
-      <div id={styles.animation}>
-        <motion.div
-          id={styles.trpfrog_name}
-          initial={{ y: 100, opacity: 0 }}
-          animate={{ y: 0, opacity: 0.7 }}
-          transition={{ delay: doAnimation ? 1.2 : 0.3, duration: 1 }}
-        >
-          Welcome!
-        </motion.div>
-        <motion.div
-          id={styles.lines}
-          initial={{ '--trpfrog-animation-start-degree': '0deg' } as any}
-          animate={{ '--trpfrog-animation-start-degree': '360deg' } as any}
-          transition={{ duration: 30, repeat: Infinity, ease: 'linear' }}
-        />
+      <div id={styles.animation} ref={ref}>
+        <div id={styles.trpfrog_name}>Welcome!</div>
+        <div id={styles.lines} />
         <div id={styles.trpfrog_image} />
+        <div>
+          <div className={styles.angle_picker_wrapper}>
+            <AnglePicker
+              size={70}
+              className={styles.angle_picker}
+              onAngleChange={degree => {
+                ref.current?.style.setProperty(
+                  '--trpfrog-animation-start-degree',
+                  degree + 'deg',
+                )
+              }}
+            />
+            <div
+              className={styles.angle_picker_button}
+              data-tooltip-id="about-angle-picker"
+            >
+              ？
+            </div>
+            <Tooltip
+              id="about-angle-picker"
+              place="right"
+              style={{ textAlign: 'left', maxWidth: 'min(50vw, 500px)' }}
+            >
+              <p style={{ margin: 0 }}>
+                <span style={{ display: 'inline-block' }}>
+                  「集中線のアニメーションのせいで端末がアチアチになる」
+                </span>
+                <span style={{ display: 'inline-block' }}>
+                  との苦情を多数いただいたため、人力アニメーションにしました。
+                </span>
+              </p>
+              <p style={{ margin: 0 }}>
+                <span style={{ display: 'inline-block' }}>
+                  アニメーションしたい方は
+                </span>
+                <span style={{ display: 'inline-block' }}>
+                  頑張って左の回すやつをくるくるしてください。
+                </span>
+              </p>
+            </Tooltip>
+          </div>
+        </div>
       </div>
-      <motion.div
-        id={id}
-        initial={doAnimation ? { y: 'calc(-1 * var(--anim-height))' } : {}}
-        animate={{ y: 0 }}
-        transition={{
-          delay: 0.4,
-          duration: 1,
-        }}
-      >
-        {children}
-      </motion.div>
+      <div id={id}>{children}</div>
     </>
   )
 }
