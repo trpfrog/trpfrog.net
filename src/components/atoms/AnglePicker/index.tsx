c89bf22e4807bfbe1f499e822ea4e91d7bebbee9
TrpFrog <dev@trpfrog.net>
2023-09-18 14:52:33 +0900

    Add imperative handle "setDegree" in AnglePicker

--- src/components/atoms/AnglePicker/index.tsx
+++ src/components/atoms/AnglePicker/index.tsx
@@ -5,63 +5,85 @@ import DivWithDragEvent from '@/components/atoms/DivWithDragEvent'
 
 import styles from './index.module.scss'
 
-type Props = Omit<
-  React.ComponentPropsWithoutRef<'div'>,
-  'children' | 'onDrag'
-> & {
+interface Props
+  extends Omit<React.ComponentPropsWithoutRef<'div'>, 'children' | 'onDrag'> {
   onAngleChange: (degree: number) => void
   size: string | number
   faceClassName?: string
+  initialDegree?: number
 }
 
-export default function AnglePicker(props: Props) {
-  const [angle, setAngle] = React.useState(0)
-  const {
-    onAngleChange,
-    size,
-    style = {},
-    className = '',
-    faceClassName = '',
-    ...rest
-  } = props
+export interface AnglePickerHandle {
+  setDegree: (degree: number | ((prev: number) => number)) => void
+}
 
-  const ref = React.useRef<HTMLDivElement>(null)
+const AnglePicker = React.forwardRef<AnglePickerHandle, Props>(
+  function AnglePicker(props, ref) {
+    const {
+      onAngleChange,
+      size,
+      style = {},
+      className = '',
+      faceClassName = '',
+      initialDegree = 0,
+      ...rest
+    } = props
+    const [degree, setDegree] = React.useState(initialDegree)
 
-  const setAngleFromMouseEvent = React.useCallback(
-    (e: MouseEvent) => {
-      const rect = ref.current?.getBoundingClientRect()
-      if (!e || !rect) {
-        return
-      }
-      const x = e.clientX - rect.left
-      const y = e.clientY - rect.top
-      const normalizedX = x / (rect.width / 2) - 1
-      const normalizedY = y / (rect.height / 2) - 1
-      const radian = Math.atan2(normalizedY, normalizedX) + Math.PI / 2
-      const degree = radian * (180 / Math.PI)
-      setAngle(degree)
-      onAngleChange?.(degree)
-    },
-    [onAngleChange],
-  )
+    const innerRef = React.useRef<HTMLDivElement>(null)
 
-  return (
-    <DivWithDragEvent
-      ref={ref}
-      className={`${styles.frame} ${className}`}
-      onDragging={setAngleFromMouseEvent}
-      style={{ width: size, height: size, ...style }}
-      {...rest}
-    >
-      <div className={`${styles.face} ${faceClassName}`} />
-      <div
-        className={styles.clock_hand}
-        style={{ transform: `rotate(${angle}deg)` } as React.CSSProperties}
+    // ref.current.setDegree で手動での角度変更をできるようにする
+    React.useImperativeHandle(ref, () => ({
+      setDegree: manualDegree => {
+        setDegree(prev => {
+          const newDegree =
+            typeof manualDegree === 'function'
+              ? manualDegree(prev)
+              : manualDegree
+          onAngleChange?.(newDegree)
+          return newDegree
+        })
+      },
+    }))
+
+    const setAngleFromMouseEvent = React.useCallback(
+      (e: MouseEvent) => {
+        const rect = innerRef.current?.getBoundingClientRect()
+        if (!e || !rect) {
+          return
+        }
+        const x = e.clientX - rect.left
+        const y = e.clientY - rect.top
+        const normalizedX = x / (rect.width / 2) - 1
+        const normalizedY = y / (rect.height / 2) - 1
+        const radian = Math.atan2(normalizedY, normalizedX) + Math.PI / 2
+        const newDegree = radian * (180 / Math.PI)
+        setDegree(newDegree)
+        onAngleChange?.(newDegree)
+      },
+      [onAngleChange],
+    )
+
+    return (
+      <DivWithDragEvent
+        ref={innerRef}
+        className={`${styles.frame} ${className}`}
+        onDragging={setAngleFromMouseEvent}
+        style={{ width: size, height: size, ...style }}
+        {...rest}
       >
-        <div className={styles.hand_dot} />
-        <div className={styles.hand} />
-        <div className={styles.clock_center} />
-      </div>
-    </DivWithDragEvent>
-  )
-}
+        <div className={`${styles.face} ${faceClassName}`} />
+        <div
+          className={styles.clock_hand}
+          style={{ transform: `rotate(${degree}deg)` } as React.CSSProperties}
+        >
+          <div className={styles.hand_dot} />
+          <div className={styles.hand} />
+          <div className={styles.clock_center} />
+        </div>
+      </DivWithDragEvent>
+    )
+  },
+)
+
+export default AnglePicker
