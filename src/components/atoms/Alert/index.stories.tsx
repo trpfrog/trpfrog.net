f200a5d191bed541298c9b651c6d61ed97907254
TrpFrog <dev@trpfrog.net>
2023-09-19 15:45:49 +0900

    Add Alert component

---
+++ src/components/atoms/Alert/index.stories.tsx
@@ -0,0 +1,28 @@
+import { LOREM_IPSUM } from '@/lib/constants'
+
+import Alert from '.'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Alert> = {
+  component: Alert,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Alert>
+
+export const Primary: Story = {
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['caution', 'info'],
+      },
+    },
+  },
+  args: {
+    type: 'caution',
+    children: LOREM_IPSUM,
+  },
+}
