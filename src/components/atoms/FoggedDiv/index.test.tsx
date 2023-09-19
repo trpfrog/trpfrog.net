2c832ae35cbc102bfeccd4896380c3b4e38521d2
TrpFrog <dev@trpfrog.net>
2023-09-19 16:57:59 +0900

    Add children rendering test (FoggedDiv)

--- src/components/atoms/FoggedDiv/index.test.tsx
+++ src/components/atoms/FoggedDiv/index.test.tsx
@@ -1,10 +1,17 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+
+import { LOREM_IPSUM } from '@/lib/constants'
 
 import { FoggedDiv } from '.'
 
-describe('ShowAll', () => {
+describe('FoggedDiv', () => {
   test('snapshot test', () => {
     const view = render(<FoggedDiv height={100} fogHeight={50} />)
     expect(view.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render correctly', () => {
+    render(<FoggedDiv height={300}>{LOREM_IPSUM}</FoggedDiv>)
+    expect(screen.getByText(LOREM_IPSUM)).toBeInTheDocument()
   })
 })
