30ed66852584b9723ad27fd598090c07c8ee55df
TrpFrog <dev@trpfrog.net>
2023-09-20 03:34:08 +0900

    Fix style of walking-result-box

--- src/app/blog/_components/article-parts/WalkingResultBox/index.tsx
+++ src/app/blog/_components/article-parts/WalkingResultBox/index.tsx
@@ -7,17 +7,20 @@ import styles from './index.module.scss'
 const WalkingResultBox: IsomorphicArticleParts = ({ content }) => {
   return (
     <div className={styles.result_box_grid}>
-      {content.split('\n').map(line => {
-        const tmp = line.split(':')
-        const title = tmp[0]
-        const value = tmp.slice(1).join(':').trim()
-        return (
-          <div key={title} className={styles.result_box}>
-            <div className={styles.result_box_title}>{title}</div>
-            <div className={styles.result_box_value}>{value}</div>
-          </div>
-        )
-      })}
+      {content
+        .split('\n')
+        .filter(Boolean)
+        .map(line => {
+          const tmp = line.split(':')
+          const title = tmp[0]
+          const value = tmp.slice(1).join(':').trim()
+          return (
+            <div key={title} className={styles.result_box}>
+              <div className={styles.result_box_title}>{title}</div>
+              <div className={styles.result_box_value}>{value}</div>
+            </div>
+          )
+        })}
     </div>
   )
 }
