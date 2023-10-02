5c85c0249312d37e944a2985e5b9f563bcbea55c
TrpFrog <dev@trpfrog.net>
2023-10-02 19:35:52 +0900

    Fix import (links/page.tsx)

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next'
 import styles from '@/app/(home)/page.module.scss'
 
 import MainWrapper from '@/components/atoms/MainWrapper'
-import Block from '@/components/molecules/Block'
+import { Block } from '@/components/molecules/Block'
 import Title from '@/components/organisms/Title'
 
 import { loadMutualLinkRecords, MutualLinkRecord } from './loader'
