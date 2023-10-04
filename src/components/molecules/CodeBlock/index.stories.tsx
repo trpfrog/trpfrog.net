62c6d65453ed66725319a55cf91ceb8bcbd6be94
TrpFrog <dev@trpfrog.net>
2023-10-04 17:14:08 +0900

    Refactor CodeBlock with ButtonWithTooltip

--- src/components/molecules/CodeBlock/index.stories.tsx
+++ src/components/molecules/CodeBlock/index.stories.tsx
@@ -10,4 +10,25 @@ export default meta
 
 type Story = StoryObj<typeof CodeBlock>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    language: 'ts',
+    fileName: 'fizzBuzz.ts',
+    children: [
+      'function fizzBuzz(n: number) {',
+      '  if (n % 15 === 0) {',
+      "    return 'FizzBuzz'",
+      '  } else if (n % 3 === 0) {',
+      "    return 'Fizz'",
+      '  } else if (n % 5 === 0) {',
+      "    return 'Buzz'",
+      '  } else {',
+      '    return n.toString()',
+      '  }',
+      '}',
+    ].join('\n'),
+  },
+}
--- src/components/molecules/CodeBlock/index.tsx
+++ src/components/molecules/CodeBlock/index.tsx
@@ -1,40 +1,19 @@
 'use client'
 
-import React, { useId } from 'react'
+import React from 'react'
 
 import { faClone } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import sh from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
-import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json'
-import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
-import html from 'react-syntax-highlighter/dist/cjs/languages/prism/markup'
-import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss'
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
-import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
-import yaml from 'react-syntax-highlighter/dist/cjs/languages/prism/yaml'
 import SyntaxHighlighter from 'react-syntax-highlighter/dist/cjs/prism-light'
 import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import { Tooltip } from 'react-tooltip'
 
+import { ButtonWithTooltip } from '@/components/atoms/ButtonWithTooltip'
+import { registerLanguages } from '@/components/molecules/CodeBlock/languages'
 import { normalizeLangName } from '@/components/molecules/CodeBlock/normalizeLangName'
 
 import styles from './index.module.scss'
 
-SyntaxHighlighter.registerLanguage('javascript', javascript)
-SyntaxHighlighter.registerLanguage('js', javascript)
-SyntaxHighlighter.registerLanguage('jsx', jsx)
-SyntaxHighlighter.registerLanguage('json', json)
-SyntaxHighlighter.registerLanguage('typescript', typescript)
-SyntaxHighlighter.registerLanguage('ts', typescript)
-SyntaxHighlighter.registerLanguage('tsx', tsx)
-SyntaxHighlighter.registerLanguage('sh', sh)
-SyntaxHighlighter.registerLanguage('shell', sh)
-SyntaxHighlighter.registerLanguage('html', html)
-SyntaxHighlighter.registerLanguage('yaml', yaml)
-SyntaxHighlighter.registerLanguage('css', css)
-SyntaxHighlighter.registerLanguage('scss', scss)
+registerLanguages()
 
 export type CodeBlockProps = Omit<
   React.ComponentPropsWithoutRef<'div'>,
@@ -48,12 +27,6 @@ export type CodeBlockProps = Omit<
 export function CodeBlock(props: CodeBlockProps) {
   const { className = '', children, language = '', fileName, ...rest } = props
 
-  const tooltipId = useId()
-  const [isCopyTooltipOpen, setIsCopyTooltipOpen] = React.useState(false)
-  const [tooltipTimeout, setTooltipTimeout] = React.useState<number | null>(
-    null,
-  )
-
   return (
     <div {...rest}>
       {language != '' && (
@@ -61,38 +34,18 @@ export function CodeBlock(props: CodeBlockProps) {
           <span className={styles.code_lang}>
             {fileName || normalizeLangName(language)}
           </span>
-          <div
+          <ButtonWithTooltip
             className={styles.copy_button}
-            data-tooltip-id={tooltipId}
             onClick={() => {
               navigator.clipboard
                 .writeText(children as string)
                 .catch(console.error)
-              if (tooltipTimeout) {
-                clearTimeout(tooltipTimeout)
-              }
-              setIsCopyTooltipOpen(true)
-              const timeoutId = window.setTimeout(() => {
-                setIsCopyTooltipOpen(false)
-              }, 2000)
-              setTooltipTimeout(timeoutId)
             }}
-            onMouseLeave={() => {
-              if (tooltipTimeout) {
-                clearTimeout(tooltipTimeout)
-              }
-              setIsCopyTooltipOpen(false)
-            }}
+            hoveredTooltipContent={'Copy'}
+            clickedTooltipContent={'Copied!'}
           >
             <FontAwesomeIcon icon={faClone} />
-          </div>
-          <Tooltip
-            id={tooltipId}
-            place={'top'}
-            style={{ padding: '0.5em', lineHeight: 1 }}
-          >
-            {isCopyTooltipOpen ? 'Copied!' : 'Copy'}
-          </Tooltip>
+          </ButtonWithTooltip>
         </div>
       )}
       <SyntaxHighlighter
