6034da5ae233cd33400885f9b98025530cdb9b3d
TrpFrog <dev@trpfrog.net>
2023-10-06 20:40:00 +0900

    Suppress alt-text warning
    Error occurred due to use of expand expression

--- src/components/utils/CldImageWrapper.tsx
+++ src/components/utils/CldImageWrapper.tsx
@@ -7,5 +7,6 @@ import Image, { ImageProps } from 'next/image'
 import { cloudinaryLoader } from '@blog/_lib/cloudinaryLoader'
 
 export function CldImageWrapper(props: Omit<ImageProps, 'loader'>) {
+  // eslint-disable-next-line jsx-a11y/alt-text
   return <Image {...props} loader={cloudinaryLoader} />
 }
