49e1557b77c2f0cb5b4c548226a85363c5332a82
TrpFrog <dev@trpfrog.net>
2023-10-07 21:16:05 +0900

    Support quote tweets (twitter-archived)

--- src/app/blog/_components/article-parts/TwitterArchive/index.tsx
+++ src/app/blog/_components/article-parts/TwitterArchive/index.tsx
@@ -1,77 +1,42 @@
 import React from 'react'
 
-import dayjs from 'dayjs'
 import { z } from 'zod'
 
 import { ErrorFallback } from '@/components/atoms/ErrorFallback'
-import { TwitterImageData } from '@/components/atoms/twitter/TwitterImage'
-import { TwitterArchived } from '@/components/organisms/TwitterArchived'
+import {
+  TwitterArchived,
+  TwitterArchivedProps,
+} from '@/components/organisms/TwitterArchived'
 
 import { IsomorphicArticleParts } from '@blog/_components/ArticleParts'
 import { parseColonSeparatedDict } from '@blog/_lib/codeBlockParser'
 
-const TwitterArchiveSchema = z.object({
-  name: z.string().default('つまみ'),
-  userid: z.string().default('TrpFrog'),
-  color: z.string().optional(),
-  tweet: z.string().optional(),
-  reply: z.string().optional(),
-  id: z.string(),
-  date: z.coerce.date().transform(date => dayjs(date).format('YYYY-MM-DD')),
+import { generateTwitterArchiveProps } from './generateTwitterArchiveProps'
 
-  image: z.string().optional(),
-  image2: z.string().optional(),
-  image3: z.string().optional(),
-  image4: z.string().optional(),
-})
+function Fallback(props: { content: string; error: z.ZodError }) {
+  if (process.env.NODE_ENV === 'development') {
+    return (
+      <ErrorFallback title={'TwitterArchive: Error Occurred'}>
+        {props.error.message}
+      </ErrorFallback>
+    )
+  } else {
+    console.error('\n```twitter-archived')
+    console.error(props.content.trim())
+    console.error('```\n')
+    throw props.error
+  }
+}
 
 export const TwitterArchive: IsomorphicArticleParts = React.memo(
   function TwitterArchive({ content }) {
     const rawTweetData = parseColonSeparatedDict(content)
-    const parsed = TwitterArchiveSchema.safeParse(rawTweetData)
-    if (!parsed.success) {
-      if (process.env.NODE_ENV === 'development') {
-        return (
-          <ErrorFallback title={'TwitterArchive: Error Occurred'}>
-            {parsed.error.message}
-          </ErrorFallback>
-        )
-      } else {
-        console.error('\n```twitter-archived')
-        console.error(content.trim())
-        console.error('```\n')
-        throw parsed.error
-      }
+    let props: TwitterArchivedProps
+    try {
+      props = generateTwitterArchiveProps(rawTweetData)
+    } catch (error) {
+      return <Fallback content={content} error={error as z.ZodError} />
     }
-
-    const tweetData = parsed.data
-
-    const images: TwitterImageData[] = [
-      tweetData.image ?? '',
-      tweetData.image2 ?? '',
-      tweetData.image3 ?? '',
-      tweetData.image4 ?? '',
-    ]
-      .filter(Boolean)
-      .map(src => ({
-        src,
-        alt: 'ツイートの画像',
-      }))
-
-    const tweet = tweetData.reply
-      ? tweetData.reply + ' ' + tweetData.tweet
-      : tweetData.tweet
-
-    return (
-      <TwitterArchived
-        author={tweetData.name}
-        screenName={tweetData.userid}
-        iconStyle={tweetData.color}
-        tweet={tweet}
-        id={tweetData.id}
-        date={tweetData.date}
-        images={images}
-      />
-    )
+    return <TwitterArchived {...props} />
   },
 )
