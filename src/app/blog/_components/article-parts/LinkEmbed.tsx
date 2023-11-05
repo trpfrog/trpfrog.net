d1a9ecdce54c70a0d38a49f95068081f52266800
TrpFrog <dev@trpfrog.net>
2023-11-06 01:48:23 +0900

    Use ClientLinkCard in linkEmbedParts

--- src/app/blog/_components/article-parts/LinkEmbed.tsx
+++ src/app/blog/_components/article-parts/LinkEmbed.tsx
@@ -1,28 +1,23 @@
 import React from 'react'
 
+import { ClientLinkCard } from '@/components/LinkCard/ClientLinkCard'
+
 import { ArticleParts } from '@blog/_components/ArticleParts'
 import { parseInlineMarkdown } from '@blog/_renderer/BlogMarkdown'
 
 export const linkEmbedParts = {
   name: 'link-embed',
-  Component: React.memo(function InnerLinkEmbed({ content }) {
+  Component: async function InnerLinkEmbed({ content }) {
     const [url, ...captionArr] = content.split('\n')
     return (
-      <div style={{ textAlign: 'center' }}>
-        <div>
-          <iframe
-            style={{
-              width: '100%',
-              height: 150,
-              maxWidth: 480,
-            }}
-            src={`https://hatenablog-parts.com/embed?url=${encodeURIComponent(
-              url.trim(),
-            )}`}
-            frameBorder="0"
-            scrolling="no"
-          />
-        </div>
+      <div style={{ display: 'grid', placeItems: 'center' }}>
+        <ClientLinkCard
+          href={url}
+          style={{
+            width: '100%',
+            maxWidth: 700,
+          }}
+        />
         {captionArr.length > 0 && (
           <div style={{ opacity: 0.8, margin: '0 0 1rem', lineHeight: 1.25 }}>
             <small>{parseInlineMarkdown(captionArr.join('\n').trim())}</small>
@@ -30,7 +25,7 @@ export const linkEmbedParts = {
         )}
       </div>
     )
-  }),
+  },
   DevComponent: React.memo(function InnerLinkEmbed({ content }) {
     return (
       <div
