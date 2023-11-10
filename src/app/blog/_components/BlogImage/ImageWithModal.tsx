36f451084cf3c46349d265338cb7871a01ae49a1
TrpFrog <dev@trpfrog.net>
2023-11-10 18:22:41 +0900

    Use image size written on markdown

--- src/app/blog/_components/BlogImage/ImageWithModal.tsx
+++ src/app/blog/_components/BlogImage/ImageWithModal.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import React, { CSSProperties, useState } from 'react'
-
-import Modal from 'react-modal'
+import React from 'react'
 
 import { CldImageWrapper } from '@/components/utils/CldImageWrapper'
 
@@ -13,75 +11,34 @@ export function ImageWithModal(props: {
   src: string
   alt?: string
   publicId?: string
-  width: number
-  height: number
 }) {
-  const srcPath = getPureCloudinaryPath(props.src)
-  const blurPath = `https://res.cloudinary.com/trpfrog/image/upload/w_10${srcPath}`
-  const modalStyle = {
-    overlay: {
-      position: 'fixed',
-      background: 'rgba(0,0,0,.8)',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      zIndex: 10,
-    } as CSSProperties,
-    content: {
-      position: 'static',
-      width: `min(calc(80vh * ${props.width / props.height}), 95vw)`,
-      height: `min(calc(95vw * ${props.height / props.width}), 80vh)`,
-      padding: 0,
-      background: 'transparent',
-      border: 'none',
-      zIndex: 10,
-    } as CSSProperties,
+  const searchParams = new URLSearchParams(props.src.split('?')[1])
+  let width = parseInt(searchParams.get('w') ?? '', 10) || 1600
+  let height = parseInt(searchParams.get('h') ?? '', 10) || 1200
+
+  if (width < 1600) {
+    width = 1600
+    height = 1600 * (height / width)
   }
 
-  const [modalState, setModalState] = useState(false)
+  const srcPath = getPureCloudinaryPath(props.src)
+  const blurPath = `https://res.cloudinary.com/trpfrog/image/upload/w_10${srcPath}`
 
-  const ImageOnArticle = () => (
+  return (
     <CldImageWrapper
       src={props.publicId ?? srcPath.slice(1)}
       alt={props.alt || props.src}
       className={`rich_image ${styles.image}`}
-      width={props.width}
-      height={props.height}
+      width={width}
+      height={height}
       quality={50}
       placeholder="blur"
       blurDataURL={blurPath}
-      onClick={() => setModalState(true)}
       sizes="100vw"
       style={{
         width: '100%',
         height: 'auto',
       }}
     />
-  )
-
-  const ImageOnModal = () => (
-    <CldImageWrapper
-      src={props.publicId ?? srcPath.slice(1)}
-      alt={props.alt || props.src}
-      className={`rich_image`}
-      width={props.width}
-      height={props.height}
-      placeholder="blur"
-      blurDataURL={blurPath}
-      sizes="100vw" // Support responsive
-    />
-  )
-
-  return (
-    <>
-      <ImageOnArticle />
-      <Modal
-        isOpen={modalState}
-        style={modalStyle}
-        onRequestClose={() => setModalState(false)}
-      >
-        <ImageOnModal />
-      </Modal>
-    </>
   )
 }
