a5e7a1a6ec41561b125096aa4079f7f6b3d7df06
TrpFrog <dev@trpfrog.net>
2023-12-13 12:41:46 +0900

    Use next/font

--- src/lib/googleFonts.ts
+++ src/lib/googleFonts.ts
@@ -1,59 +1,69 @@
-// import {
-//   BIZ_UDPGothic,
-//   Comfortaa,
-//   M_PLUS_Rounded_1c,
-//   Noto_Sans_JP,
-//   Noto_Sans_Mono,
-//   Roboto
-// } from "next/font/google";
-// import {NextFontWithVariable} from "next/dist/compiled/@next/font";
-//
-// const mPlusRounded1c = M_PLUS_Rounded_1c({
-//   subsets: ['latin'],
-//   weight: ["100", "400", "700", "800"],
-//   display: 'swap',
-//   variable: '--font-m-plus-rounded-1c',
-// })
-//
-// const comfortaa = Comfortaa({
-//   subsets: ['latin'],
-//   variable: '--font-comfortaa',
-// })
-//
-// const roboto = Roboto({
-//   subsets: ['latin'],
-//   weight: ["400", "700"],
-//   variable: '--font-roboto',
-// })
-//
-// const notoSansJP = Noto_Sans_JP({
-//   subsets: ['latin'],
-//   weight: ["400", "700"],
-//   variable: '--font-noto-sans-jp',
-// })
-//
-// const notoSansMono = Noto_Sans_Mono({
-//   subsets: ['latin'],
-//   variable: '--font-noto-sans-mono',
-// })
-//
-// const bizUdpGothic = BIZ_UDPGothic({
-//   subsets: ['latin'],
-//   weight: ["400", "700"],
-//   variable: '--font-biz-udp-gothic',
-// })
-//
-// const fonts: NextFontWithVariable[] = [
-//   mPlusRounded1c,
-//   comfortaa,
-//   roboto,
-//   notoSansJP,
-//   notoSansMono,
-//   bizUdpGothic,
-// ]
-//
-// const fontVariables = fonts.map((font) => font.variable).join(' ');
+import { NextFontWithVariable } from 'next/dist/compiled/@next/font'
+import {
+  BIZ_UDPGothic,
+  Comfortaa,
+  Inconsolata,
+  M_PLUS_Rounded_1c,
+  Noto_Sans_JP,
+  Noto_Sans_Mono,
+  Roboto,
+} from 'next/font/google'
 
-// Temporary solution for the next/font issue
-import styles from '@/components/head/GoogleFonts/index.module.scss'
-export const fontVariables = styles.temp_google_font_variables
+const mPlusRounded1c = M_PLUS_Rounded_1c({
+  subsets: ['latin'],
+  weight: ['100', '400', '700', '800'],
+  display: 'swap',
+  variable: '--font-m-plus-rounded-1c',
+  // To avoid "Failed to find font override values" error,
+  // set adjustFontFallback to false
+  adjustFontFallback: false,
+})
+
+const comfortaa = Comfortaa({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-comfortaa',
+})
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+})
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-noto-sans-jp',
+})
+
+const notoSansMono = Noto_Sans_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-noto-sans-mono',
+})
+
+const bizUdpGothic = BIZ_UDPGothic({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-biz-udp-gothic',
+})
+
+const inconsolata = Inconsolata({
+  subsets: ['latin'],
+  weight: ['200'],
+  variable: '--font-inconsolata',
+})
+
+// Font loaders must be called and assigned to a const in the module scope
+const fonts: NextFontWithVariable[] = [
+  mPlusRounded1c,
+  comfortaa,
+  roboto,
+  notoSansJP,
+  notoSansMono,
+  bizUdpGothic,
+  inconsolata,
+]
+
+export const fontVariables = fonts.map(font => font.variable).join(' ')
