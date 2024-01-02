db2b54d03136e3289eddbde1818c6fcf3968ea8d
TrpFrog <dev@trpfrog.net>
2024-01-03 04:39:41 +0900

    Add CompetitiveCard

--- src/app/(home)/_cards/CompetitiveCard.tsx
+++ src/app/(home)/_cards/CompetitiveCard.tsx
@@ -1,5 +1,36 @@
-import { TopCard } from '@/app/(home)/_components/TopCard'
+import { LinkTopCard } from '@/app/(home)/_components/TopCard'
+
+import { tv } from '@/lib/tailwind/variants'
+
+const styles = tv({
+  slots: {
+    wrapper: [
+      'tw-flex tw-w-full tw-flex-col tw-items-center tw-justify-center',
+      'tw-bg-zinc-800 tw-py-3',
+    ],
+    skill: 'tw-flex tw-flex-wrap tw-gap-2',
+    textGradient: [
+      'tw-flex tw-w-full tw-flex-col tw-items-center tw-justify-center',
+      'tw-bg-gradient-to-br tw-from-cyan-100 tw-via-cyan-500 tw-to-cyan-100 tw-bg-clip-text',
+      'tw-font-bold tw-text-transparent',
+    ],
+    rating: 'tw-text-7xl tw-font-bold ',
+  },
+})()
 
 export function CompetitiveCard() {
-  return <TopCard>AtCoder</TopCard>
+  return (
+    <LinkTopCard
+      className={styles.wrapper()}
+      href="https://atcoder.jp/users/TrpFrog"
+    >
+      <div className={styles.textGradient()}>
+        <div className={styles.rating()}>
+          <span className="-tw-mr-1 tw-text-3xl">max</span>
+          1596
+        </div>
+        <div>AtCoder 水色レーティング</div>
+      </div>
+    </LinkTopCard>
+  )
 }
