eff4ddadae8c7e86f82e3d664ab5e99b1a5b7bec
TrpFrog <dev@trpfrog.net>
2024-01-02 17:15:52 +0900

    Add clickable style (PlainBlock)

--- src/components/atoms/PlainBlock.tsx
+++ src/components/atoms/PlainBlock.tsx
@@ -9,6 +9,18 @@ export const plainBlockStyle = tv({
     'tw-overflow-clip tw-rounded-3xl',
     'print:tw-border-2 print:tw-border-black print:tw-shadow-gray-400',
   ],
+  variants: {
+    clickable: {
+      true: `
+        tw-cursor-pointer hover:-tw-translate-y-1 hover:tw-shadow-[0_8px_0]
+        hover:tw-shadow-trpfrog-400 active:tw-translate-y-[4px]
+        active:tw-shadow-none hover:dark:tw-shadow-trpfrog-900
+      `,
+    },
+  },
+  defaultVariants: {
+    clickable: false,
+  },
 })
 
 export type PlainBlockProps = React.ComponentPropsWithRef<'div'>
