3d91193eb1c80b8b6d24503158b22cfa11d9503c
TrpFrog <dev@trpfrog.net>
2024-01-03 21:20:26 +0900

    Tweak design (Walking)

--- src/app/(home)/_cards/WalkingCard.tsx
+++ src/app/(home)/_cards/WalkingCard.tsx
@@ -1,21 +1,21 @@
 import { tv } from 'tailwind-variants'
 
-import { TopCard } from '@/app/(home)/_components/TopCard'
+import { LinkTopCard } from '@/app/(home)/_components/TopCard'
 import { cardButtonStyle } from '@/app/(home)/_styles/cardButtonStyle'
-
-import { A } from '@/components/wrappers'
 
 const backgroundImage =
   'https://res.cloudinary.com/trpfrog/blog/tokyotower-walking/20210324231643'
 
 const styles = tv({
   slots: {
-    bg: 'tw-bg-cover tw-bg-center tw-bg-no-repeat',
+    bg: 'tw-bg-cover tw-bg-center tw-bg-no-repeat sp:tw-h-32',
     wrapper: [
-      'tw-h-full tw-w-full tw-backdrop-brightness-50',
-      'tw-px-4 tw-py-2 tw-text-sm tw-text-white',
+      'tw-flex tw-flex-col tw-items-center tw-justify-between',
+      'tw-bg-gradient-to-b tw-from-zinc-800 tw-via-transparent tw-to-zinc-800',
+      'tw-h-full tw-w-full tw-p-3',
+      'tw-text-center tw-text-xs tw-text-white tw-opacity-90',
     ],
-    h2: 'tw-mb-1 tw-text-2xl tw-font-bold',
+    h2: 'tw-mb-1 tw-text-3xl tw-font-bold',
     button: cardButtonStyle({
       className: 'tw-absolute tw-bottom-3 tw-right-4',
     }),
@@ -24,17 +24,19 @@ const styles = tv({
 
 export function WalkingCard() {
   return (
-    <TopCard
+    <LinkTopCard
+      href="/walking"
       className={styles.bg()}
       style={{ backgroundImage: `url('${backgroundImage}')` }}
     >
       <div className={styles.wrapper()}>
         <h2 className={styles.h2()}>Walking</h2>
-        <div>たまに長距離を歩いて移動して記事を書いています。(最長 70.5km)</div>
-        <A href="/walking" className={styles.button()}>
-          もっと見る
-        </A>
+        <div>
+          たまに長距離を歩いて移動して
+          <br />
+          記事を書いています。(最長 70.5km)
+        </div>
       </div>
-    </TopCard>
+    </LinkTopCard>
   )
 }
