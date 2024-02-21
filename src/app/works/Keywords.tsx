f475ce1e97df606941732c069f3e1ef1dd1f382f
TrpFrog <dev@trpfrog.net>
2024-02-22 02:57:57 +0900

    Add icons on works

--- src/app/works/Keywords.tsx
+++ src/app/works/Keywords.tsx
@@ -1,4 +1,5 @@
 import { tv } from '@/lib/tailwind/variants'
+import 'devicon'
 
 const styles = tv({
   slots: {
@@ -16,16 +17,50 @@ interface Props {
   keywords: string[]
 }
 
+export const technologyIconMap: Record<string, string> = {
+  Python: 'devicon-python-plain',
+  PyTorch: 'devicon-pytorch-original',
+  TypeScript: 'devicon-typescript-plain',
+  'Google Cloud': 'devicon-googlecloud-plain',
+  'Twitter API': 'devicon-twitter-original',
+  Twitter4J: 'devicon-twitter-original',
+  React: 'devicon-react-original',
+  'Next.js': 'devicon-nextjs-plain',
+  'Node.js': 'devicon-nodejs-plain',
+  JavaScript: 'devicon-javascript-plain',
+  OpenGL: 'devicon-opengl-plain',
+  'C++': 'devicon-cplusplus-plain',
+  HTML: 'devicon-html5-plain',
+  CSS: 'devicon-css3-plain',
+  Docker: 'devicon-docker-plain',
+  Processing: 'devicon-processing-plain',
+  Java: 'devicon-java-plain',
+  'C programming': 'devicon-c-plain',
+  SCSS: 'devicon-sass-original',
+}
+
 export function Keywords({ keywords }: Props) {
   return (
     <div className={styles.wrapper()}>
       <h3 className={styles.title()}>TECHNOLOGIES</h3>
       <ul className={styles.keywords()}>
-        {keywords.map(k => (
-          <li key={k} className={styles.keyword()}>
-            {k}
-          </li>
-        ))}
+        {keywords.map(k =>
+          k in technologyIconMap ? (
+            <li key={k} className={styles.keyword()}>
+              <span
+                className={`${technologyIconMap[k]} tw-relative tw-top-[1px] tw-mr-1 tw-hidden dark:tw-inline`}
+              />
+              <span
+                className={`${technologyIconMap[k]} colored tw-relative tw-top-[1px] tw-mr-1 dark:tw-hidden`}
+              />
+              {k}
+            </li>
+          ) : (
+            <li key={k} className={styles.keyword()}>
+              {k}
+            </li>
+          ),
+        )}
       </ul>
     </div>
   )
