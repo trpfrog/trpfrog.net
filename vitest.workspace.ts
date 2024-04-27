9142700be84874b349015752ee7ffda95318cc0a
TrpFrog <dev@trpfrog.net>
2024-04-27 15:20:11 +0900

    Add config-vitest package

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,20 +1,3 @@
-import react from '@vitejs/plugin-react'
 import { defineWorkspace } from 'vitest/config'
 
-export default defineWorkspace([
-  'apps/*',
-  'packages/*',
-  {
-    extends: './vitest.config.ts',
-    plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'happy-dom',
-      css: {
-        modules: {
-          classNameStrategy: 'non-scoped',
-        },
-      },
-    },
-  },
-])
+export default defineWorkspace(['apps/*', 'packages/*'])
