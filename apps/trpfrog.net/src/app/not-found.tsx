8458935f5ff22459e673248937a16bd2b5e39645
TrpFrog <dev@trpfrog.net>
2024-04-29 01:19:36 +0900

    Fix style of not-found.tsx

--- apps/trpfrog.net/src/app/not-found.tsx
+++ apps/trpfrog.net/src/app/not-found.tsx
@@ -1,14 +1,11 @@
-import Image from 'next/legacy/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import { RichButton } from 'src/components/atoms/RichButton'
 
 import { MainWrapper } from '@/components/atoms/MainWrapper'
 import { Title } from '@/components/organisms/Title'
 
-
-const IB = ({ children }: any) => (
-  <span className="tw-inline-block">{children}</span>
-)
+const IB = ({ children }: any) => <span className="tw-inline-block">{children}</span>
 
 export default function NotFound() {
   return (
@@ -19,13 +16,11 @@ export default function NotFound() {
         style={{ textAlign: 'center', padding: '3rem 0' }}
       >
         <h1 style={{ textAlign: 'center' }}>
-          <div style={{ fontSize: '3em', color: 'var(--header-color)' }}>
-            404
-          </div>
+          <div style={{ fontSize: '3em', color: 'var(--header-color)' }}>404</div>
           <div style={{ fontSize: '0.9em' }}>Not Found</div>
         </h1>
-        <div style={{ marginTop: '1rem' }}>
-          <Image src={'404'} width={300} height={300} alt={'404の画像'} />
+        <div className="tw-mt-4 tw-grid tw-place-items-center">
+          <Image src="/images/404.png" width={300} height={300} alt="" />
         </div>
         <div style={{ padding: '0 1em' }}>
           <p>
