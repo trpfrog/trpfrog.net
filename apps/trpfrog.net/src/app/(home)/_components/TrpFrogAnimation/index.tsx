37e3ae237c613f5da083e36c35e344afb9a91196
TrpFrog <dev@trpfrog.net>
2024-05-05 04:24:56 +0900

    Revert TrpFrogAnimation

--- apps/trpfrog.net/src/app/(home)/_components/TrpFrogAnimation/index.tsx
+++ apps/trpfrog.net/src/app/(home)/_components/TrpFrogAnimation/index.tsx
@@ -11,8 +11,6 @@ import { AnglePicker } from '@/components/atoms/AnglePicker'
 
 import styles from './index.module.scss'
 
-import { useIsKawaiiLogo } from '@/states/kawaiiLogoAtom.ts'
-
 type Props = {
   children?: React.ReactNode
   id?: string
@@ -38,27 +36,14 @@ export function TrpFrogAnimation({ children, id }: Props) {
   const [maxRpm, setMaxRpm] = useState(0)
   const showMaxRpm = maxRpm >= 150
 
-  const isKawaii = useIsKawaiiLogo()
-
   return (
     <>
       <div id={styles.animation} ref={ref}>
-        {!isKawaii && (
-          <div id={styles.trpfrog_name} className={showMaxRpm ? styles.current_rpm : ''}>
-            {showMaxRpm ? `${rpm.toFixed(0)}rpm` : 'Welcome!'}
-          </div>
-        )}
+        <div id={styles.trpfrog_name} className={showMaxRpm ? styles.current_rpm : ''}>
+          {showMaxRpm ? `${rpm.toFixed(0)}rpm` : 'Welcome!'}
+        </div>
         <div id={styles.lines} />
-        {!isKawaii && <div className={styles.trpfrog_image} data-turn-eyes={rpm > 180} />}
-        {isKawaii && (
-          <div className="tw-absolute tw-inset-0 tw-grid tw-place-items-center">
-            <img
-              src="/images/kawaii.svg"
-              alt=""
-              className="tw-w-[min(90vw,700px)] -tw-translate-y-4"
-            />
-          </div>
-        )}
+        <div className={styles.trpfrog_image} data-turn-eyes={rpm > 180} />
         <div>
           <div className={styles.angle_picker_wrapper}>
             <AnglePicker
