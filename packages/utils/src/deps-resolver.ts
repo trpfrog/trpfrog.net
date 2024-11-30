8daa069b17cf586ef7827d3fd4583524a54a020b
TrpFrog <dev@trpfrog.net>
2024-12-01 02:11:58 +0900

    Update deps-resolver.ts

--- packages/utils/src/deps-resolver.ts
+++ packages/utils/src/deps-resolver.ts
@@ -1,9 +1,21 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-explicit-any -- Allow `any` for creating utility types */
 
 // Prettify type to make the type more readable
 type Prettify<T> = {
   [K in keyof T]: T[K]
 } & {}
+
+type PartialSelectedKeys<T, K extends keyof T> = Prettify<Omit<T, K> & Partial<Pick<T, K>>>
+
+type PartialIfAllChildrenAreOptional<T extends Record<string, Record<string, unknown>>> =
+  PartialSelectedKeys<
+    T,
+    keyof {
+      // FIXME: なんか条件式に T[K] が含まれているとエラーになる
+      [K in keyof T as Partial<T[K]> extends T[K] ? K : never]: null
+    }
+  >
 
 type Internal_OptionalValueIsNotSet = '__internal_optionalValueIsNotSet__'
 
@@ -14,22 +26,22 @@ type Internal_OptionalValueIsNotSet = '__internal_optionalValueIsNotSet__'
  * @returns
  */
 export function createDepsResolver<
-  Fns extends Record<string, (deps: any) => any>,
-  DefaultDeps extends Partial<{ [K in keyof Fns]: Partial<Parameters<Fns[K]>[0]> }>,
+  Fns extends Record<string, (deps: any) => unknown>,
+  DepsDict extends { [K in keyof Fns]: Parameters<Fns[K]>[0] },
+  DefaultDeps extends Partial<{ [K in keyof DepsDict]: Partial<DepsDict[K]> }>,
+  // prettier-ignore
+  Args extends Prettify<
+    PartialIfAllChildrenAreOptional<{
+      [K in keyof DepsDict]:
+        // Requires all keys from Fns[K] except those in DefaultDeps[K]
+        Omit<DepsDict[K], keyof DefaultDeps[K]> &
+        // Allows all keys from DefaultDeps[K] to be optional
+        Partial<Pick<DepsDict[K], keyof DefaultDeps[K]>>
+    }>
+  >,
+  Returns extends Prettify<{ [K in keyof Fns]: ReturnType<Fns[K]> }>,
 >(fns: Fns, defaultDeps?: DefaultDeps) {
   defaultDeps = defaultDeps ?? ({} as DefaultDeps)
-  type AllDeps = { [K in keyof Fns]: Parameters<Fns[K]>[0] }
-
-  // prettier-ignore
-  type Args = {
-    [K in keyof Fns]: Prettify<
-      // Requires all keys from Fns[K] except those in DefaultDeps[K]
-      Omit<AllDeps[K], keyof DefaultDeps[K]>
-      // Allows all keys from DefaultDeps[K] to be optional
-      & Partial<Pick<AllDeps[K], keyof DefaultDeps[K]>>
-    >
-  }
-  type Returns = { [K in keyof Fns]: ReturnType<Fns[K]> }
 
   return {
     defaultDeps,
@@ -38,7 +50,9 @@ export function createDepsResolver<
       Object.fromEntries(
         Object.entries(fns).map(([key, fn]) => [
           key,
-          fn({ ...(defaultDeps[key] ?? {}), ...deps[key] }),
+          // @ts-expect-error - Error due to mismatch between Args index signature type and string key type,
+          // but ignored due to heavy use of type hacks.
+          fn({ ...(defaultDeps[key] ?? {}), ...(deps[key] ?? {}) }),
         ]),
       ) as any,
   }
@@ -60,12 +74,14 @@ export function createSingleDepsResolver<
     },
     {
       usecase: (defaultDeps ?? {}) as Internal_OptionalValueIsNotSet extends DefaultDeps
-        ? // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-          {}
+        ? {}
         : DefaultDeps,
     },
   )
+
   return {
+    // FIXME: Fix the type error
+    // @ts-expect-error - Error due to mismatch between Args index signature type and string key type,
     resolve: (deps: Parameters<typeof resolve>[0]['usecase']) => resolve({ usecase: deps }).usecase,
     defaultDeps: defaultDeps2.usecase,
   }
