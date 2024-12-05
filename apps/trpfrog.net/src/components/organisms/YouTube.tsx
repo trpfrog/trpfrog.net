7c27ca36ccfd8c467327ba1e71160abc1f98ab14
TrpFrog <dev@trpfrog.net>
2024-12-06 04:00:21 +0900

    Support params in YouTube blog component

--- apps/trpfrog.net/src/components/organisms/YouTube.tsx
+++ apps/trpfrog.net/src/components/organisms/YouTube.tsx
@@ -4,8 +4,11 @@ import { YouTubeEmbed } from '@next/third-parties/google'
 
 interface YouTubeProps {
   videoId: string
+  params?: string
 }
 
-export const YouTube = memo(function YouTube({ videoId }: YouTubeProps) {
-  return <YouTubeEmbed videoid={videoId} style="border-radius: 5px; margin: 0 auto" />
+export const YouTube = memo(function YouTube({ videoId, params }: YouTubeProps) {
+  return (
+    <YouTubeEmbed videoid={videoId} params={params} style="border-radius: 5px; margin: 0 auto" />
+  )
 })
