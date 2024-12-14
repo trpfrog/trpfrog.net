f9eb407c6e504a02b5a0454bf10bfb14c4ce677a
tsmami <dev@trpfrog.net>
2024-12-15 04:15:36 +0900

    Fix R2 key

--- apps/image-generation/src/infra/repos/imageStoreRepoCloudflareR2.ts
+++ apps/image-generation/src/infra/repos/imageStoreRepoCloudflareR2.ts
@@ -3,21 +3,21 @@ import { getContext } from 'hono/context-storage'
 import { ImageStoreRepo } from '../../domain/repos/image-store-repo'
 import { Env } from '../../env'
 
-function toFilePath(filename: string) {
-  return `/ai-icons/${filename}`
+function toR2Key(filename: string) {
+  return `ai-icons/${filename}`
 }
 
 export const imageStoreRepoCloudflareR2: ImageStoreRepo = {
   upload: async (filename: string, imageData: ArrayBuffer) => {
     const c = getContext<Env>()
-    const filePath = toFilePath(filename)
+    const filePath = toR2Key(filename)
     await c.env.BUCKET.put(filePath, imageData)
-    return `https://media.trpfrog.net${filePath}`
+    return `https://media.trpfrog.net/${filePath}`
   },
 
   download: async (filename: string) => {
     const c = getContext<Env>()
-    const filePath = toFilePath(filename)
+    const filePath = toR2Key(filename)
     const res = await c.env.BUCKET.get(filePath)
     if (res === null) {
       throw new Error('Not found')
@@ -27,7 +27,7 @@ export const imageStoreRepoCloudflareR2: ImageStoreRepo = {
 
   delete: async (filename: string) => {
     const c = getContext<Env>()
-    const filePath = toFilePath(filename)
+    const filePath = toR2Key(filename)
     await c.env.BUCKET.delete(filePath)
   },
 }
