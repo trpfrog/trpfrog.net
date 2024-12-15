784d1ecc1e6354ea73bc240d407ef4a425c39c62
tsmami <dev@trpfrog.net>
2024-12-15 19:43:20 +0900

    Specify model name

--- apps/image-generation/src/index.ts
+++ apps/image-generation/src/index.ts
@@ -13,7 +13,7 @@ const app = createApp(env => {
     hfToken: env.HUGGINGFACE_TOKEN,
   })
   const jsonChatbot = createOpenAIChatLLMJson({
-    model: 'gpt-4o-mini',
+    model: 'gpt-4o-mini-2024-07-18',
     temperature: 0.9,
     apiKey: env.OPENAI_API_KEY,
   })
