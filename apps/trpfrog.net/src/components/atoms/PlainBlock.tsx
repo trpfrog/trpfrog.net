9080bed76169b5ebd1e20a91211a781f036f4381
tsmami <dev@trpfrog.net>
2024-12-20 15:55:27 +0900

    Tweak design

--- apps/trpfrog.net/src/components/atoms/PlainBlock.tsx
+++ apps/trpfrog.net/src/components/atoms/PlainBlock.tsx
@@ -8,10 +8,14 @@ export const plainBlockStyle = tv({
     'tw-shadow-trpfrog-400 dark:tw-shadow-trpfrog-900',
     'tw-overflow-clip tw-rounded-[--window-border-radius]',
     'print:tw-border-2 print:tw-border-black print:tw-shadow-gray-400',
+    'focus-visible:tw-ring-8',
   ],
   variants: {
     clickable: {
-      true: `tw-cursor-pointer hover:-tw-translate-y-1 hover:tw-shadow-[0_8px_0] hover:tw-shadow-trpfrog-400 active:tw-translate-y-[3px] active:tw-shadow-none hover:dark:tw-shadow-trpfrog-900`,
+      true: [
+        'tw-cursor-pointer hover:-tw-translate-y-1 active:tw-translate-y-[3px] active:tw-shadow-none',
+        'hover:tw-shadow-[0_8px_0] hover:tw-shadow-trpfrog-400 hover:dark:tw-shadow-trpfrog-900',
+      ],
     },
   },
   defaultVariants: {
