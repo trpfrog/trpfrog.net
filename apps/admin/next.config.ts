93fbb7a441f09efe3da3222841ec146773e08084
tsmami <dev@trpfrog.net>
2024-12-23 18:12:33 +0900

    Setup Mantine

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 }
 
 export default nextConfig
