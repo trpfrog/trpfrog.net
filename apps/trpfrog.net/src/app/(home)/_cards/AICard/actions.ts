7d6aff74e28e96e1ec01ff54d49ff87103877195
tsmami <dev@trpfrog.net>
2024-12-28 01:58:51 +0900

    Add random delay to icon update requests to reduce simultaneous requests

--- apps/trpfrog.net/src/app/(home)/_cards/AICard/actions.ts
+++ apps/trpfrog.net/src/app/(home)/_cards/AICard/actions.ts
@@ -7,11 +7,19 @@ import { env } from '@/env/server'
 
 const client = createTrpFrogImageGenerationClient('production')
 
+function promiseTimeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 export async function requestUpdateIcon() {
   const apiKey = env.TRPFROG_FUNCTIONS_SECRET
   if (apiKey == null) {
     forbidden()
   }
+
+  // ランダムに 0 〜 5 秒待ち、同時にリクエストを送りにくくする
+  // 画像更新リクエストを投げているだけなので、遅延によるUXへの影響は考えなくて良い
+  await promiseTimeout(Math.random() * 5000)
 
   await client.update.$post({
     query: {},
