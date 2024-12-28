edc8a12a77a5dfd4431e997010dabe41bf8e8d16
tsmami <dev@trpfrog.net>
2024-12-29 00:07:19 +0900

    Update year endpoint to return the current year in Tokyo timezone

--- apps/trpfrog.net/src/app/api/[[...route]]/_routes/tmpCache.ts
+++ apps/trpfrog.net/src/app/api/[[...route]]/_routes/tmpCache.ts
@@ -3,7 +3,7 @@ import { differenceInYears } from 'date-fns'
 import { Hono } from 'hono'
 
 export const app = new Hono()
-  .get('/year', c => c.text(new Date().getFullYear().toString()))
+  .get('/year', c => c.text(new TZDate(new Date(), 'Asia/Tokyo').getFullYear().toString()))
   .get('/age', c => {
     const birthday = new TZDate('2000-10-17', 'Asia/Tokyo')
     const today = new Date()
