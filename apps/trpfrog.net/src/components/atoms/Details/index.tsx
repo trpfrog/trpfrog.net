c51e978c975b5895c4154068c230855f9350f288
tsmami <dev@trpfrog.net>
2025-01-05 22:01:04 +0900

    Add LazyDetails

--- apps/trpfrog.net/src/components/atoms/Details/index.tsx
+++ apps/trpfrog.net/src/components/atoms/Details/index.tsx
@@ -2,16 +2,17 @@ import * as React from 'react'
 
 import styles from './index.module.css'
 
-type DetailsProps = React.ComponentPropsWithoutRef<'details'> & {
+export type DetailsProps = React.ComponentPropsWithoutRef<'details'> & {
   summary: string
   innerProps?: Omit<React.ComponentPropsWithoutRef<'div'>, 'children'>
+  onClick?: (event: React.MouseEvent<HTMLDetailsElement, MouseEvent>) => void
 }
 
 export function Details(props: DetailsProps) {
-  const { className = '', children, summary, ...rest } = props
+  const { className = '', children, summary, onClick, ...rest } = props
   return (
     <details className={`${styles.details} ${className}`} {...rest}>
-      <summary>{summary}</summary>
+      <summary onClick={onClick}>{summary}</summary>
       <div {...props.innerProps}>{children}</div>
     </details>
   )
