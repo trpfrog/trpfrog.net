5b2ba46b0472aca7ef6acafc783c72e68f18ee5d
tsmami <dev@trpfrog.net>
2026-01-02 16:10:41 +0900

    chore(content-server): set dev source strategy env config
    - run dev with realtime source strategy
    - validate source strategy env variable

--- apps/content-server/src/env.ts
+++ apps/content-server/src/env.ts
@@ -1,5 +1,19 @@
+import { validateUnknown } from '@trpfrog.net/utils'
+import * as v from 'valibot'
+
 import { Bindings } from '../worker-configuration'
 
 export type Variables = Record<string, never>
 
 export type Env = { Bindings: Bindings; Variables: Record<string, string> & Variables }
+
+export const env = validateUnknown(
+  v.object({
+    TRPFROG_NET_CONTENT_SERVER_SOURCE_STRATEGY: v.optional(
+      v.picklist(['dev-realtime', 'static-generated']),
+      'static-generated',
+    ),
+  }),
+  // eslint-disable-next-line n/no-process-env
+  process.env,
+)
